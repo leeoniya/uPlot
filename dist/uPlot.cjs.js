leeoniya/uPlot

commit 562200b7cc9b463b0fc57ec56b1a0c302572bb90

reduce rounding errors in cursor point positioning

dist/uPlot.cjs.js
@@ -263,6 +263,10 @@ function incrRoundDn(num, incr) {
 	return floor(num/incr)*incr;
 }
 
+function round2(val) {
+	return round(val * 1e2) / 1e2;
+}
+
 function round6(val) {
 	return round(val * 1e6) / 1e6;
 }
@@ -758,12 +762,12 @@ function getYPos(val, scale, hgt) {
 		{ return val; }
 
 	var pctY = (val - scale.min) / (scale.max - scale.min);
-	return round((1 - pctY) * hgt);
+	return (1 - pctY) * hgt;
 }
 
 function getXPos(val, scale, wid) {
 	var pctX = (val - scale.min) / (scale.max - scale.min);
-	return round(pctX * wid);
+	return pctX * wid;
 }
 
 function snapNone(self, dataMin, dataMax) {
@@ -1220,7 +1224,7 @@ function Line(opts, data) {
 				ctx.stroke(path);
 
 				if (s.fill != null) {
-					var zeroY = getYPos(0, scales[s.scale], can[HEIGHT]);
+					var zeroY = round(getYPos(0, scales[s.scale], can[HEIGHT]));
 
 					path.lineTo(can[WIDTH], zeroY);
 					path.lineTo(0, zeroY);
@@ -1251,12 +1255,12 @@ function Line(opts, data) {
 		var _i0 = clamp(i0 - 1, 0, dataLen - 1);
 		var _i1 = clamp(i1 + 1, 0, dataLen - 1);
 
-		var prevX = getXPos(xdata[dir == 1 ? _i0 : _i1], scaleX, can[WIDTH]),
+		var prevX = round(getXPos(xdata[dir == 1 ? _i0 : _i1], scaleX, can[WIDTH])),
 			prevY;
 
 		for (var i = dir == 1 ? _i0 : _i1; dir == 1 ? i <= _i1 : i >= _i0; i += dir) {
-			x = getXPos(xdata[i], scaleX, can[WIDTH]);
-			y = getYPos(ydata[i], scaleY, can[HEIGHT]);
+			x = round(getXPos(xdata[i], scaleX, can[WIDTH]));
+			y = round(getYPos(ydata[i], scaleY, can[HEIGHT]));
 
 			if (dir == -1 && i == _i1)
 				{ path.lineTo(x, y); }
@@ -1333,7 +1337,7 @@ function Line(opts, data) {
 			var cssProp = ori == 0 ? LEFT : TOP;
 
 			// TODO: filter ticks & offsets that will end up off-canvas
-			var canOffs = ticks.map(function (val) { return getPos(val, scale, can[dim]); });		// bit of waste if we're not drawing a grid
+			var canOffs = ticks.map(function (val) { return round2(getPos(val, scale, can[dim])); });		// bit of waste if we're not drawing a grid
 
 			var labels = axis.values(self, scale.type == 2 ? ticks.map(function (i) { return data0[i]; }) : ticks, space);		// BOO this assumes a specific data/series
 
@@ -1660,7 +1664,7 @@ function Line(opts, data) {
 
 	self.posToIdx = closestIdxFromXpos;
 	self.posToVal = function (pos, scale) { return scaleValueAtPos(scale == xScaleKey ? pos : canCssHeight - pos, scale); };
-	self.valToPos = function (val, scale) { return (scale == xScaleKey ? getXPos(val, scales[scale], canCssWidth) : getYPos(val, scales[scale], canCssHeight)); };
+	self.valToPos = function (val, scale) { return (scale == xScaleKey ? round(getXPos(val, scales[scale], canCssWidth)) : round(getYPos(val, scales[scale], canCssHeight))); };
 
 	var inBatch = false;
 	var shouldPaint = false;
@@ -1699,8 +1703,8 @@ function Line(opts, data) {
 		rafPending = false;
 
 		if (cursor.show && cursor.cross) {
-			trans(vt,mouseLeft1,0);
-			trans(hz,0,mouseTop1);
+			trans(vt,round(mouseLeft1),0);
+			trans(hz,0,round(mouseTop1));
 		}
 
 		var idx;
@@ -1731,13 +1735,13 @@ function Line(opts, data) {
 
 			var scX = scales[xScaleKey];
 
-			var xPos = getXPos(data[0][idx], scX, canCssWidth);
+			var xPos = round2(getXPos(data[0][idx], scX, canCssWidth));
 
 			for (var i$1 = 0; i$1 < series.length; i$1++) {
 				var s = series[i$1];
 
 				if (i$1 > 0 && s.show) {
-					var yPos = getYPos(data[i$1][idx], scales[s.scale], canCssHeight);
+					var yPos = round2(getYPos(data[i$1][idx], scales[s.scale], canCssHeight));
 
 					if (yPos == null)
 						{ yPos = -10; }

dist/uPlot.iife.js
@@ -264,6 +264,10 @@ var uPlot = (function (exports) {
 		return floor(num/incr)*incr;
 	}
 
+	function round2(val) {
+		return round(val * 1e2) / 1e2;
+	}
+
 	function round6(val) {
 		return round(val * 1e6) / 1e6;
 	}
@@ -759,12 +763,12 @@ var uPlot = (function (exports) {
 			{ return val; }
 
 		var pctY = (val - scale.min) / (scale.max - scale.min);
-		return round((1 - pctY) * hgt);
+		return (1 - pctY) * hgt;
 	}
 
 	function getXPos(val, scale, wid) {
 		var pctX = (val - scale.min) / (scale.max - scale.min);
-		return round(pctX * wid);
+		return pctX * wid;
 	}
 
 	function snapNone(self, dataMin, dataMax) {
@@ -1221,7 +1225,7 @@ var uPlot = (function (exports) {
 					ctx.stroke(path);
 
 					if (s.fill != null) {
-						var zeroY = getYPos(0, scales[s.scale], can[HEIGHT]);
+						var zeroY = round(getYPos(0, scales[s.scale], can[HEIGHT]));
 
 						path.lineTo(can[WIDTH], zeroY);
 						path.lineTo(0, zeroY);
@@ -1252,12 +1256,12 @@ var uPlot = (function (exports) {
 			var _i0 = clamp(i0 - 1, 0, dataLen - 1);
 			var _i1 = clamp(i1 + 1, 0, dataLen - 1);
 
-			var prevX = getXPos(xdata[dir == 1 ? _i0 : _i1], scaleX, can[WIDTH]),
+			var prevX = round(getXPos(xdata[dir == 1 ? _i0 : _i1], scaleX, can[WIDTH])),
 				prevY;
 
 			for (var i = dir == 1 ? _i0 : _i1; dir == 1 ? i <= _i1 : i >= _i0; i += dir) {
-				x = getXPos(xdata[i], scaleX, can[WIDTH]);
-				y = getYPos(ydata[i], scaleY, can[HEIGHT]);
+				x = round(getXPos(xdata[i], scaleX, can[WIDTH]));
+				y = round(getYPos(ydata[i], scaleY, can[HEIGHT]));
 
 				if (dir == -1 && i == _i1)
 					{ path.lineTo(x, y); }
@@ -1334,7 +1338,7 @@ var uPlot = (function (exports) {
 				var cssProp = ori == 0 ? LEFT : TOP;
 
 				// TODO: filter ticks & offsets that will end up off-canvas
-				var canOffs = ticks.map(function (val) { return getPos(val, scale, can[dim]); });		// bit of waste if we're not drawing a grid
+				var canOffs = ticks.map(function (val) { return round2(getPos(val, scale, can[dim])); });		// bit of waste if we're not drawing a grid
 
 				var labels = axis.values(self, scale.type == 2 ? ticks.map(function (i) { return data0[i]; }) : ticks, space);		// BOO this assumes a specific data/series
 
@@ -1661,7 +1665,7 @@ var uPlot = (function (exports) {
 
 		self.posToIdx = closestIdxFromXpos;
 		self.posToVal = function (pos, scale) { return scaleValueAtPos(scale == xScaleKey ? pos : canCssHeight - pos, scale); };
-		self.valToPos = function (val, scale) { return (scale == xScaleKey ? getXPos(val, scales[scale], canCssWidth) : getYPos(val, scales[scale], canCssHeight)); };
+		self.valToPos = function (val, scale) { return (scale == xScaleKey ? round(getXPos(val, scales[scale], canCssWidth)) : round(getYPos(val, scales[scale], canCssHeight))); };
 
 		var inBatch = false;
 		var shouldPaint = false;
@@ -1700,8 +1704,8 @@ var uPlot = (function (exports) {
 			rafPending = false;
 
 			if (cursor.show && cursor.cross) {
-				trans(vt,mouseLeft1,0);
-				trans(hz,0,mouseTop1);
+				trans(vt,round(mouseLeft1),0);
+				trans(hz,0,round(mouseTop1));
 			}
 
 			var idx;
@@ -1732,13 +1736,13 @@ var uPlot = (function (exports) {
 
 				var scX = scales[xScaleKey];
 
-				var xPos = getXPos(data[0][idx], scX, canCssWidth);
+				var xPos = round2(getXPos(data[0][idx], scX, canCssWidth));
 
 				for (var i$1 = 0; i$1 < series.length; i$1++) {
 					var s = series[i$1];
 
 					if (i$1 > 0 && s.show) {
-						var yPos = getYPos(data[i$1][idx], scales[s.scale], canCssHeight);
+						var yPos = round2(getYPos(data[i$1][idx], scales[s.scale], canCssHeight));
 
 						if (yPos == null)
 							{ yPos = -10; }

src/Line.js
@@ -5,6 +5,7 @@ import {
 	abs,
 	floor,
 	round,
+	round2,
 	ceil,
 	min,
 	max,
@@ -109,12 +110,12 @@ function getYPos(val, scale, hgt) {
 		return val;
 
 	let pctY = (val - scale.min) / (scale.max - scale.min);
-	return round((1 - pctY) * hgt);
+	return (1 - pctY) * hgt;
 }
 
 function getXPos(val, scale, wid) {
 	let pctX = (val - scale.min) / (scale.max - scale.min);
-	return round(pctX * wid);
+	return pctX * wid;
 }
 
 function snapNone(self, dataMin, dataMax) {
@@ -569,7 +570,7 @@ export function Line(opts, data) {
 				ctx.stroke(path);
 
 				if (s.fill != null) {
-					let zeroY = getYPos(0, scales[s.scale], can[HEIGHT]);
+					let zeroY = round(getYPos(0, scales[s.scale], can[HEIGHT]));
 
 					path.lineTo(can[WIDTH], zeroY);
 					path.lineTo(0, zeroY);
@@ -600,12 +601,12 @@ export function Line(opts, data) {
 		let _i0 = clamp(i0 - 1, 0, dataLen - 1);
 		let _i1 = clamp(i1 + 1, 0, dataLen - 1);
 
-		let prevX = getXPos(xdata[dir == 1 ? _i0 : _i1], scaleX, can[WIDTH]),
+		let prevX = round(getXPos(xdata[dir == 1 ? _i0 : _i1], scaleX, can[WIDTH])),
 			prevY;
 
 		for (let i = dir == 1 ? _i0 : _i1; dir == 1 ? i <= _i1 : i >= _i0; i += dir) {
-			x = getXPos(xdata[i], scaleX, can[WIDTH]);
-			y = getYPos(ydata[i], scaleY, can[HEIGHT]);
+			x = round(getXPos(xdata[i], scaleX, can[WIDTH]));
+			y = round(getYPos(ydata[i], scaleY, can[HEIGHT]));
 
 			if (dir == -1 && i == _i1)
 				path.lineTo(x, y);
@@ -679,7 +680,7 @@ export function Line(opts, data) {
 			let cssProp = ori == 0 ? LEFT : TOP;
 
 			// TODO: filter ticks & offsets that will end up off-canvas
-			let canOffs = ticks.map(val => getPos(val, scale, can[dim]));		// bit of waste if we're not drawing a grid
+			let canOffs = ticks.map(val => round2(getPos(val, scale, can[dim])));		// bit of waste if we're not drawing a grid
 
 			let labels = axis.values(self, scale.type == 2 ? ticks.map(i => data0[i]) : ticks, space);		// BOO this assumes a specific data/series
 
@@ -1006,7 +1007,7 @@ export function Line(opts, data) {
 
 	self.posToIdx = closestIdxFromXpos;
 	self.posToVal = (pos, scale) => scaleValueAtPos(scale == xScaleKey ? pos : canCssHeight - pos, scale);
-	self.valToPos = (val, scale) => (scale == xScaleKey ? getXPos(val, scales[scale], canCssWidth) : getYPos(val, scales[scale], canCssHeight));
+	self.valToPos = (val, scale) => (scale == xScaleKey ? round(getXPos(val, scales[scale], canCssWidth)) : round(getYPos(val, scales[scale], canCssHeight)));
 
 	let inBatch = false;
 	let shouldPaint = false;
@@ -1045,8 +1046,8 @@ export function Line(opts, data) {
 		rafPending = false;
 
 		if (cursor.show && cursor.cross) {
-			trans(vt,mouseLeft1,0);
-			trans(hz,0,mouseTop1);
+			trans(vt,round(mouseLeft1),0);
+			trans(hz,0,round(mouseTop1));
 		}
 
 		let idx;
@@ -1077,13 +1078,13 @@ export function Line(opts, data) {
 
 			let scX = scales[xScaleKey];
 
-			let xPos = getXPos(data[0][idx], scX, canCssWidth);
+			let xPos = round2(getXPos(data[0][idx], scX, canCssWidth));
 
 			for (let i = 0; i < series.length; i++) {
 				let s = series[i];
 
 				if (i > 0 && s.show) {
-					let yPos = getYPos(data[i][idx], scales[s.scale], canCssHeight);
+					let yPos = round2(getYPos(data[i][idx], scales[s.scale], canCssHeight));
 
 					if (yPos == null)
 						yPos = -10;

commit bd216f243243bd96080c11d50439f4d2a8fc9d8c

fix missing data (regressed by 562200b: "reduce rounding errors in cursor point positioning")

demos/missing-data.html
@@ -9,7 +9,7 @@
 		<link rel="stylesheet" href="../src/uPlot.css">
 	</head>
 	<body>
-		<script src="../dist/uPlot.iife.min.js"></script>
+		<script src="../dist/uPlot.iife.js"></script>
 		<h2 id="wait">Loading lib....</h2>
 
 		<script>

dist/uPlot.cjs.js
@@ -754,9 +754,6 @@ function splitXY(d) {
 }
 
 function getYPos(val, scale, hgt) {
-	if (val == null)
-		{ return val; }
-
 	var pctY = (val - scale.min) / (scale.max - scale.min);
 	return (1 - pctY) * hgt;
 }
@@ -1253,7 +1250,7 @@ function Line(opts, data) {
 			if (dir == -1 && i == _i1)
 				{ path.lineTo(x, y); }
 
-			if (y == null)
+			if (ydata[i] == null)
 				{ gap = true; }
 			else {
 				if ((dir == 1 ? x - prevX : prevX - x) >= width) {

dist/uPlot.iife.js
@@ -755,9 +755,6 @@ var uPlot = (function (exports) {
 	}
 
 	function getYPos(val, scale, hgt) {
-		if (val == null)
-			{ return val; }
-
 		var pctY = (val - scale.min) / (scale.max - scale.min);
 		return (1 - pctY) * hgt;
 	}
@@ -1254,7 +1251,7 @@ var uPlot = (function (exports) {
 				if (dir == -1 && i == _i1)
 					{ path.lineTo(x, y); }
 
-				if (y == null)
+				if (ydata[i] == null)
 					{ gap = true; }
 				else {
 					if ((dir == 1 ? x - prevX : prevX - x) >= width) {

src/Line.js
@@ -106,9 +106,6 @@ function splitXY(d) {
 }
 
 function getYPos(val, scale, hgt) {
-	if (val == null)
-		return val;
-
 	let pctY = (val - scale.min) / (scale.max - scale.min);
 	return (1 - pctY) * hgt;
 }
@@ -602,7 +599,7 @@ export function Line(opts, data) {
 			if (dir == -1 && i == _i1)
 				path.lineTo(x, y);
 
-			if (y == null)
+			if (ydata[i] == null)
 				gap = true;
 			else {
 				if ((dir == 1 ? x - prevX : prevX - x) >= width) {