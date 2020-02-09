leeoniya/uPlot

commit 2248c199ba21c8ce2ed9fc60fcc6863a38fcd039

snap mouse to edges when within 1px. close #123.

dist/uPlot.cjs.js
@@ -245,11 +245,9 @@ var log10 = M.log10;
 
 var inf = Infinity;
 
-/*
-export function incrRound() {
+function incrRound(num, incr) {
 	return round(num/incr)*incr;
 }
-*/
 
 function clamp(num, _min, _max) {
 	return min(max(num, _min), _max);
@@ -2071,6 +2069,12 @@ function Line(opts, data, then) {
 			_x = canCssWidth * (_x/_w);
 			_y = canCssHeight * (_y/_h);
 		}
+
+		if (_x <= 1 || _x >= canCssWidth - 1)
+			{ _x = incrRound(_x, canCssWidth); }
+
+		if (_y <= 1 || _y >= canCssHeight - 1)
+			{ _y = incrRound(_y, canCssHeight); }
 
 		if (initial) {
 			mouseLeft0 = _x;

dist/uPlot.iife.js
@@ -246,11 +246,9 @@ var uPlot = (function (exports) {
 
 	var inf = Infinity;
 
-	/*
-	export function incrRound() {
+	function incrRound(num, incr) {
 		return round(num/incr)*incr;
 	}
-	*/
 
 	function clamp(num, _min, _max) {
 		return min(max(num, _min), _max);
@@ -2072,6 +2070,12 @@ var uPlot = (function (exports) {
 				_x = canCssWidth * (_x/_w);
 				_y = canCssHeight * (_y/_h);
 			}
+
+			if (_x <= 1 || _x >= canCssWidth - 1)
+				{ _x = incrRound(_x, canCssWidth); }
+
+			if (_y <= 1 || _y >= canCssHeight - 1)
+				{ _y = incrRound(_y, canCssHeight); }
 
 			if (initial) {
 				mouseLeft0 = _x;

src/Line.js
@@ -18,6 +18,7 @@ import {
 	rangeNum,
 	incrRoundUp,
 	incrRoundDn,
+	incrRound,
 	assign,
 	isArr,
 	isStr,
@@ -1363,6 +1364,12 @@ export function Line(opts, data, then) {
 			_x = canCssWidth * (_x/_w);
 			_y = canCssHeight * (_y/_h);
 		}
+
+		if (_x <= 1 || _x >= canCssWidth - 1)
+			_x = incrRound(_x, canCssWidth);
+
+		if (_y <= 1 || _y >= canCssHeight - 1)
+			_y = incrRound(_y, canCssHeight);
 
 		if (initial) {
 			mouseLeft0 = _x;

src/utils.js
@@ -104,11 +104,9 @@ export const log10 = M.log10;
 
 export const inf = Infinity;
 
-/*
-export function incrRound() {
+export function incrRound(num, incr) {
 	return round(num/incr)*incr;
 }
-*/
 
 export function clamp(num, _min, _max) {
 	return min(max(num, _min), _max);

commit 0cd531f078d866758f742bc021da9cadf2010f4a

fix synced cursor not hiding on mouseleave. (regressed by 2248c19: edge snapping)

dist/uPlot.cjs.js
@@ -2064,7 +2064,7 @@ function Line(opts, data, then) {
 		if (rect == null)
 			{ syncRect(); }
 
-		cacheMouse(e, src, _x, _y, _w, _h, _i, false);
+		cacheMouse(e, src, _x, _y, _w, _h, _i, false, e != null);
 
 		if (e != null) {
 			if (cursorRaf == 0)
@@ -2074,7 +2074,7 @@ function Line(opts, data, then) {
 			{ updateCursor(); }
 	}
 
-	function cacheMouse(e, src, _x, _y, _w, _h, _i, initial) {
+	function cacheMouse(e, src, _x, _y, _w, _h, _i, initial, snap) {
 		if (e != null) {
 			_x = e.clientX - rect.left;
 			_y = e.clientY - rect.top;
@@ -2084,11 +2084,13 @@ function Line(opts, data, then) {
 			_y = canCssHeight * (_y/_h);
 		}
 
-		if (_x <= 1 || _x >= canCssWidth - 1)
-			{ _x = incrRound(_x, canCssWidth); }
+		if (snap) {
+			if (_x <= 1 || _x >= canCssWidth - 1)
+				{ _x = incrRound(_x, canCssWidth); }
 
-		if (_y <= 1 || _y >= canCssHeight - 1)
-			{ _y = incrRound(_y, canCssHeight); }
+			if (_y <= 1 || _y >= canCssHeight - 1)
+				{ _y = incrRound(_y, canCssHeight); }
+		}
 
 		if (initial) {
 			mouseLeft0 = _x;
@@ -2111,7 +2113,7 @@ function Line(opts, data, then) {
 		if (e == null || filtMouse(e)) {
 			dragging = true;
 
-			cacheMouse(e, src, _x, _y, _w, _h, _i, true);
+			cacheMouse(e, src, _x, _y, _w, _h, _i, true, true);
 
 			if (drag.x || drag.y)
 				{ hideSelect(); }
@@ -2127,7 +2129,7 @@ function Line(opts, data, then) {
 		if ((e == null || filtMouse(e))) {
 			dragging = false;
 
-			cacheMouse(e, src, _x, _y, _w, _h, _i, false);
+			cacheMouse(e, src, _x, _y, _w, _h, _i, false, true);
 
 			if (mouseLeft1 != mouseLeft0 || mouseTop1 != mouseTop0) {
 				setSelect(_select);

dist/uPlot.iife.js
@@ -2065,7 +2065,7 @@ var uPlot = (function (exports) {
 			if (rect == null)
 				{ syncRect(); }
 
-			cacheMouse(e, src, _x, _y, _w, _h, _i, false);
+			cacheMouse(e, src, _x, _y, _w, _h, _i, false, e != null);
 
 			if (e != null) {
 				if (cursorRaf == 0)
@@ -2075,7 +2075,7 @@ var uPlot = (function (exports) {
 				{ updateCursor(); }
 		}
 
-		function cacheMouse(e, src, _x, _y, _w, _h, _i, initial) {
+		function cacheMouse(e, src, _x, _y, _w, _h, _i, initial, snap) {
 			if (e != null) {
 				_x = e.clientX - rect.left;
 				_y = e.clientY - rect.top;
@@ -2085,11 +2085,13 @@ var uPlot = (function (exports) {
 				_y = canCssHeight * (_y/_h);
 			}
 
-			if (_x <= 1 || _x >= canCssWidth - 1)
-				{ _x = incrRound(_x, canCssWidth); }
+			if (snap) {
+				if (_x <= 1 || _x >= canCssWidth - 1)
+					{ _x = incrRound(_x, canCssWidth); }
 
-			if (_y <= 1 || _y >= canCssHeight - 1)
-				{ _y = incrRound(_y, canCssHeight); }
+				if (_y <= 1 || _y >= canCssHeight - 1)
+					{ _y = incrRound(_y, canCssHeight); }
+			}
 
 			if (initial) {
 				mouseLeft0 = _x;
@@ -2112,7 +2114,7 @@ var uPlot = (function (exports) {
 			if (e == null || filtMouse(e)) {
 				dragging = true;
 
-				cacheMouse(e, src, _x, _y, _w, _h, _i, true);
+				cacheMouse(e, src, _x, _y, _w, _h, _i, true, true);
 
 				if (drag.x || drag.y)
 					{ hideSelect(); }
@@ -2128,7 +2130,7 @@ var uPlot = (function (exports) {
 			if ((e == null || filtMouse(e))) {
 				dragging = false;
 
-				cacheMouse(e, src, _x, _y, _w, _h, _i, false);
+				cacheMouse(e, src, _x, _y, _w, _h, _i, false, true);
 
 				if (mouseLeft1 != mouseLeft0 || mouseTop1 != mouseTop0) {
 					setSelect(_select);

src/Line.js
@@ -1356,7 +1356,7 @@ export function Line(opts, data, then) {
 		if (rect == null)
 			syncRect();
 
-		cacheMouse(e, src, _x, _y, _w, _h, _i, false);
+		cacheMouse(e, src, _x, _y, _w, _h, _i, false, e != null);
 
 		if (e != null) {
 			if (cursorRaf == 0)
@@ -1366,7 +1366,7 @@ export function Line(opts, data, then) {
 			updateCursor();
 	}
 
-	function cacheMouse(e, src, _x, _y, _w, _h, _i, initial) {
+	function cacheMouse(e, src, _x, _y, _w, _h, _i, initial, snap) {
 		if (e != null) {
 			_x = e.clientX - rect.left;
 			_y = e.clientY - rect.top;
@@ -1376,11 +1376,13 @@ export function Line(opts, data, then) {
 			_y = canCssHeight * (_y/_h);
 		}
 
-		if (_x <= 1 || _x >= canCssWidth - 1)
-			_x = incrRound(_x, canCssWidth);
+		if (snap) {
+			if (_x <= 1 || _x >= canCssWidth - 1)
+				_x = incrRound(_x, canCssWidth);
 
-		if (_y <= 1 || _y >= canCssHeight - 1)
-			_y = incrRound(_y, canCssHeight);
+			if (_y <= 1 || _y >= canCssHeight - 1)
+				_y = incrRound(_y, canCssHeight);
+		}
 
 		if (initial) {
 			mouseLeft0 = _x;
@@ -1403,7 +1405,7 @@ export function Line(opts, data, then) {
 		if (e == null || filtMouse(e)) {
 			dragging = true;
 
-			cacheMouse(e, src, _x, _y, _w, _h, _i, true);
+			cacheMouse(e, src, _x, _y, _w, _h, _i, true, true);
 
 			if (drag.x || drag.y)
 				hideSelect();
@@ -1419,7 +1421,7 @@ export function Line(opts, data, then) {
 		if ((e == null || filtMouse(e))) {
 			dragging = false;
 
-			cacheMouse(e, src, _x, _y, _w, _h, _i, false);
+			cacheMouse(e, src, _x, _y, _w, _h, _i, false, true);
 
 			if (mouseLeft1 != mouseLeft0 || mouseTop1 != mouseTop0) {
 				setSelect(_select);