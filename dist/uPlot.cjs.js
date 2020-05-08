leeoniya/uPlot

commit c82ce09b2b1e6d9391e2922e8c758aff27be84c1

pass foundIncr to axis.values() to handle case of a single split. close #200.

dist/uPlot.cjs.js
@@ -518,8 +518,7 @@ var _timeAxisStamps = [
 // TODO: will need to accept spaces[] and pull incr into the loop when grid will be non-uniform, eg for log scales.
 // currently we ignore this for months since they're *nearly* uniform and the added complexity is not worth it
 function timeAxisVals(tzDate, stamps) {
-	return function (self, splits, space) {
-		var incr = round3(splits[1] - splits[0]);
+	return function (self, splits, space, incr) {
 		var s = stamps.find(function (e) { return incr >= e[0]; });
 
 		// these track boundaries when a full label is needed again
@@ -733,7 +732,7 @@ var xSeriesOpts = {
 // alternative: https://stackoverflow.com/a/2254896
 var fmtNum = new Intl.NumberFormat(navigator.language);
 
-function numAxisVals(self, splits, space) {
+function numAxisVals(self, splits, space, incr) {
 	return splits.map(fmtNum.format);
 }
 
@@ -1884,7 +1883,7 @@ function uPlot(opts, data, then) {
 			var tickSize = ticks.show ? round(ticks.size * pxRatio) : 0;
 
 			// tick labels
-			var values = axis.values(self, scale.distr == 2 ? splits.map(function (i) { return data0[i]; }) : splits, space);		// BOO this assumes a specific data/series
+			var values = axis.values(self, scale.distr == 2 ? splits.map(function (i) { return data0[i]; }) : splits, space, incr);		// BOO this assumes a specific data/series
 
 			// rotating of labels only supported on bottom x axis
 			var angle = side == 2 ? axis.rotate(self, values, space) * -PI/180 : 0;

dist/uPlot.esm.js
@@ -521,8 +521,7 @@ const _timeAxisStamps = [
 // TODO: will need to accept spaces[] and pull incr into the loop when grid will be non-uniform, eg for log scales.
 // currently we ignore this for months since they're *nearly* uniform and the added complexity is not worth it
 function timeAxisVals(tzDate, stamps) {
-	return (self, splits, space) => {
-		let incr = round3(splits[1] - splits[0]);
+	return (self, splits, space, incr) => {
 		let s = stamps.find(e => incr >= e[0]);
 
 		// these track boundaries when a full label is needed again
@@ -736,7 +735,7 @@ const xSeriesOpts = {
 // alternative: https://stackoverflow.com/a/2254896
 let fmtNum = new Intl.NumberFormat(navigator.language);
 
-function numAxisVals(self, splits, space) {
+function numAxisVals(self, splits, space, incr) {
 	return splits.map(fmtNum.format);
 }
 
@@ -1876,7 +1875,7 @@ function uPlot(opts, data, then) {
 			let tickSize = ticks.show ? round(ticks.size * pxRatio) : 0;
 
 			// tick labels
-			let values = axis.values(self, scale.distr == 2 ? splits.map(i => data0[i]) : splits, space);		// BOO this assumes a specific data/series
+			let values = axis.values(self, scale.distr == 2 ? splits.map(i => data0[i]) : splits, space, incr);		// BOO this assumes a specific data/series
 
 			// rotating of labels only supported on bottom x axis
 			let angle = side == 2 ? axis.rotate(self, values, space) * -PI/180 : 0;

dist/uPlot.iife.js
@@ -519,8 +519,7 @@ var uPlot = (function () {
 	// TODO: will need to accept spaces[] and pull incr into the loop when grid will be non-uniform, eg for log scales.
 	// currently we ignore this for months since they're *nearly* uniform and the added complexity is not worth it
 	function timeAxisVals(tzDate, stamps) {
-		return function (self, splits, space) {
-			var incr = round3(splits[1] - splits[0]);
+		return function (self, splits, space, incr) {
 			var s = stamps.find(function (e) { return incr >= e[0]; });
 
 			// these track boundaries when a full label is needed again
@@ -734,7 +733,7 @@ var uPlot = (function () {
 	// alternative: https://stackoverflow.com/a/2254896
 	var fmtNum = new Intl.NumberFormat(navigator.language);
 
-	function numAxisVals(self, splits, space) {
+	function numAxisVals(self, splits, space, incr) {
 		return splits.map(fmtNum.format);
 	}
 
@@ -1885,7 +1884,7 @@ var uPlot = (function () {
 				var tickSize = ticks.show ? round(ticks.size * pxRatio) : 0;
 
 				// tick labels
-				var values = axis.values(self, scale.distr == 2 ? splits.map(function (i) { return data0[i]; }) : splits, space);		// BOO this assumes a specific data/series
+				var values = axis.values(self, scale.distr == 2 ? splits.map(function (i) { return data0[i]; }) : splits, space, incr);		// BOO this assumes a specific data/series
 
 				// rotating of labels only supported on bottom x axis
 				var angle = side == 2 ? axis.rotate(self, values, space) * -PI/180 : 0;

src/uPlot.js
@@ -1141,7 +1141,7 @@ export default function uPlot(opts, data, then) {
 			let tickSize = ticks.show ? round(ticks.size * pxRatio) : 0;
 
 			// tick labels
-			let values = axis.values(self, scale.distr == 2 ? splits.map(i => data0[i]) : splits, space);		// BOO this assumes a specific data/series
+			let values = axis.values(self, scale.distr == 2 ? splits.map(i => data0[i]) : splits, space, incr);		// BOO this assumes a specific data/series
 
 			// rotating of labels only supported on bottom x axis
 			let angle = side == 2 ? axis.rotate(self, values, space) * -PI/180 : 0;

dist/uPlot.esm.d.ts
@@ -395,7 +395,7 @@ declare namespace uPlot {
 		split?: number[] | ((self: uPlot, scaleMin: number, scaleMax: number, foundIncr: number, pctSpace: number) => number[]);
 
 		/** formats splits values for rendering */
-		values?: (self: uPlot, splits: number[], foundSpace: number) => Array<string|number>;
+		values?: (self: uPlot, splits: number[], foundSpace: number, foundIncr: number) => Array<string|number>;
 
 		/** values rotation in degrees off horizontal (only bottom axes w/ side: 2) */
 		rotate?: number | ((self: uPlot, values: Array<string|number>, foundSpace: number) => number);

src/opts.js
@@ -153,8 +153,7 @@ export const _timeAxisStamps = [
 // TODO: will need to accept spaces[] and pull incr into the loop when grid will be non-uniform, eg for log scales.
 // currently we ignore this for months since they're *nearly* uniform and the added complexity is not worth it
 export function timeAxisVals(tzDate, stamps) {
-	return (self, splits, space) => {
-		let incr = round3(splits[1] - splits[0]);
+	return (self, splits, space, incr) => {
 		let s = stamps.find(e => incr >= e[0]);
 
 		// these track boundaries when a full label is needed again
@@ -369,7 +368,7 @@ export const xSeriesOpts = {
 // alternative: https://stackoverflow.com/a/2254896
 let fmtNum = new Intl.NumberFormat(navigator.language);
 
-export function numAxisVals(self, splits, space) {
+export function numAxisVals(self, splits, space, incr) {
 	return splits.map(fmtNum.format);
 }
 

commit 8cdb3b6e5f463c3c6828076d327ffa0dacf84f04

pass proper foundIncr when scale.distr = 2. (regressed by c82ce09)

dist/uPlot.cjs.js
@@ -1883,7 +1883,13 @@ function uPlot(opts, data, then) {
 			var tickSize = ticks.show ? round(ticks.size * pxRatio) : 0;
 
 			// tick labels
-			var values = axis.values(self, scale.distr == 2 ? splits.map(function (i) { return data0[i]; }) : splits, space, incr);		// BOO this assumes a specific data/series
+			// BOO this assumes a specific data/series
+			var values = axis.values(
+				self,
+				scale.distr == 2 ? splits.map(function (i) { return data0[i]; }) : splits,
+				space,
+				scale.distr == 2 ? data0[splits[1]] -  data0[splits[0]] : incr
+			);
 
 			// rotating of labels only supported on bottom x axis
 			var angle = side == 2 ? axis.rotate(self, values, space) * -PI/180 : 0;

dist/uPlot.esm.js
@@ -1875,7 +1875,13 @@ function uPlot(opts, data, then) {
 			let tickSize = ticks.show ? round(ticks.size * pxRatio) : 0;
 
 			// tick labels
-			let values = axis.values(self, scale.distr == 2 ? splits.map(i => data0[i]) : splits, space, incr);		// BOO this assumes a specific data/series
+			// BOO this assumes a specific data/series
+			let values = axis.values(
+				self,
+				scale.distr == 2 ? splits.map(i => data0[i]) : splits,
+				space,
+				scale.distr == 2 ? data0[splits[1]] -  data0[splits[0]] : incr,
+			);
 
 			// rotating of labels only supported on bottom x axis
 			let angle = side == 2 ? axis.rotate(self, values, space) * -PI/180 : 0;

dist/uPlot.iife.js
@@ -1884,7 +1884,13 @@ var uPlot = (function () {
 				var tickSize = ticks.show ? round(ticks.size * pxRatio) : 0;
 
 				// tick labels
-				var values = axis.values(self, scale.distr == 2 ? splits.map(function (i) { return data0[i]; }) : splits, space, incr);		// BOO this assumes a specific data/series
+				// BOO this assumes a specific data/series
+				var values = axis.values(
+					self,
+					scale.distr == 2 ? splits.map(function (i) { return data0[i]; }) : splits,
+					space,
+					scale.distr == 2 ? data0[splits[1]] -  data0[splits[0]] : incr
+				);
 
 				// rotating of labels only supported on bottom x axis
 				var angle = side == 2 ? axis.rotate(self, values, space) * -PI/180 : 0;

src/uPlot.js
@@ -1141,7 +1141,13 @@ export default function uPlot(opts, data, then) {
 			let tickSize = ticks.show ? round(ticks.size * pxRatio) : 0;
 
 			// tick labels
-			let values = axis.values(self, scale.distr == 2 ? splits.map(i => data0[i]) : splits, space, incr);		// BOO this assumes a specific data/series
+			// BOO this assumes a specific data/series
+			let values = axis.values(
+				self,
+				scale.distr == 2 ? splits.map(i => data0[i]) : splits,
+				space,
+				scale.distr == 2 ? data0[splits[1]] -  data0[splits[0]] : incr,
+			);
 
 			// rotating of labels only supported on bottom x axis
 			let angle = side == 2 ? axis.rotate(self, values, space) * -PI/180 : 0;