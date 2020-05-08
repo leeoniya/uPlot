f9f7bb55970780b940e864863c075895056e8335
Klaster_1 <Klaster1@gmail.com>
2020-05-08 20:46:39 +0300

    Rename SpanGap to DataGap.

--- dist/uPlot.esm.d.ts
+++ dist/uPlot.esm.d.ts
@@ -276,7 +276,7 @@ declare namespace uPlot {
 		max?: number,
 	}
 
-	export type SpanGap = [number, number]
+	export type DataGap = [number, number]
 
 	export interface Series {
 		/** series on/off. when off, it will not affect its scale */
@@ -289,7 +289,7 @@ declare namespace uPlot {
 		scale?: string;
 
 		/** when true, null data values will not cause line breaks. when fn, should filter and return gaps to span */
-		spanGaps?: boolean | ((self: uPlot, foundGaps: Array<SpanGap>, seriesIdx: number) => Array<SpanGap>);
+		spanGaps?: boolean | ((self: uPlot, foundGaps: Array<DataGap>, seriesIdx: number) => Array<DataGap>);
 
 		/** legend label */
 		label?: string;
