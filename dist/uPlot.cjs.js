leeoniya/uPlot

commit 2ffebba392d029ad3dd7df63deffbb5cc3b74ab2

add points.space to simplify point density control

dist/uPlot.cjs.js
@@ -971,8 +971,9 @@ function ptDia(width, mult) {
 }
 
 function seriesPoints(self, si) {
-	var dia = ptDia(self.series[si].width, pxRatio);
-	var maxPts = self.bbox.width / dia / 2;
+	var s = self.series[si];
+	var dia = ptDia(s.width, pxRatio);
+	var maxPts = self.bbox.width / s.points.space;
 	var idxs = self.series[0].idxs;
 	return idxs[1] - idxs[0] <= maxPts;
 }
@@ -1485,6 +1486,7 @@ function uPlot(opts, data, then) {
 			s.points = assign({}, {
 				size: _ptDia,
 				width: max(1, _ptDia * .2),
+				space: _ptDia * 2,
 			}, s.points);
 			s.points.show = fnOrSelf(s.points.show);
 			s._paths = null;

dist/uPlot.esm.js
@@ -974,8 +974,9 @@ function ptDia(width, mult) {
 }
 
 function seriesPoints(self, si) {
-	const dia = ptDia(self.series[si].width, pxRatio);
-	let maxPts = self.bbox.width / dia / 2;
+	const s = self.series[si];
+	const dia = ptDia(s.width, pxRatio);
+	let maxPts = self.bbox.width / s.points.space;
 	let idxs = self.series[0].idxs;
 	return idxs[1] - idxs[0] <= maxPts;
 }
@@ -1484,6 +1485,7 @@ function uPlot(opts, data, then) {
 			s.points = assign({}, {
 				size: _ptDia,
 				width: max(1, _ptDia * .2),
+				space: _ptDia * 2,
 			}, s.points);
 			s.points.show = fnOrSelf(s.points.show);
 			s._paths = null;

dist/uPlot.iife.js
@@ -972,8 +972,9 @@ var uPlot = (function () {
 	}
 
 	function seriesPoints(self, si) {
-		var dia = ptDia(self.series[si].width, pxRatio);
-		var maxPts = self.bbox.width / dia / 2;
+		var s = self.series[si];
+		var dia = ptDia(s.width, pxRatio);
+		var maxPts = self.bbox.width / s.points.space;
 		var idxs = self.series[0].idxs;
 		return idxs[1] - idxs[0] <= maxPts;
 	}
@@ -1486,6 +1487,7 @@ var uPlot = (function () {
 				s.points = assign({}, {
 					size: _ptDia,
 					width: max(1, _ptDia * .2),
+					space: _ptDia * 2,
 				}, s.points);
 				s.points.show = fnOrSelf(s.points.show);
 				s._paths = null;

src/opts.js
@@ -510,8 +510,9 @@ export function ptDia(width, mult) {
 }
 
 function seriesPoints(self, si) {
-	const dia = ptDia(self.series[si].width, pxRatio);
-	let maxPts = self.bbox.width / dia / 2;
+	const s = self.series[si];
+	const dia = ptDia(s.width, pxRatio);
+	let maxPts = self.bbox.width / s.points.space;
 	let idxs = self.series[0].idxs;
 	return idxs[1] - idxs[0] <= maxPts;
 }

dist/uPlot.d.ts
@@ -359,6 +359,9 @@ declare namespace uPlot {
 			/** diameter of point in CSS pixels */
 			size?: number;
 
+			/** minimum avg space between point centers before they're shown (default: size * 2) */
+			space: number;
+
 			/** line width of circle outline in CSS pixels */
 			width?: CanvasRenderingContext2D['lineWidth'];
 

src/uPlot.js
@@ -576,6 +576,7 @@ export default function uPlot(opts, data, then) {
 			s.points = assign({}, {
 				size: _ptDia,
 				width: max(1, _ptDia * .2),
+				space: _ptDia * 2,
 			}, s.points);
 			s.points.show = fnOrSelf(s.points.show);
 			s._paths = null;

commit 14f44f277b77075db7b30e4c6ea80172ba4d8da8

fix point density calc regressed by 2ffebba

dist/uPlot.cjs.js
@@ -973,7 +973,7 @@ function ptDia(width, mult) {
 function seriesPoints(self, si) {
 	var s = self.series[si];
 	var dia = ptDia(s.width, pxRatio);
-	var maxPts = self.bbox.width / s.points.space;
+	var maxPts = self.bbox.width / (s.points.space * pxRatio);
 	var idxs = self.series[0].idxs;
 	return idxs[1] - idxs[0] <= maxPts;
 }

dist/uPlot.esm.js
@@ -976,7 +976,7 @@ function ptDia(width, mult) {
 function seriesPoints(self, si) {
 	const s = self.series[si];
 	const dia = ptDia(s.width, pxRatio);
-	let maxPts = self.bbox.width / s.points.space;
+	let maxPts = self.bbox.width / (s.points.space * pxRatio);
 	let idxs = self.series[0].idxs;
 	return idxs[1] - idxs[0] <= maxPts;
 }

dist/uPlot.iife.js
@@ -974,7 +974,7 @@ var uPlot = (function () {
 	function seriesPoints(self, si) {
 		var s = self.series[si];
 		var dia = ptDia(s.width, pxRatio);
-		var maxPts = self.bbox.width / s.points.space;
+		var maxPts = self.bbox.width / (s.points.space * pxRatio);
 		var idxs = self.series[0].idxs;
 		return idxs[1] - idxs[0] <= maxPts;
 	}

src/opts.js
@@ -512,7 +512,7 @@ export function ptDia(width, mult) {
 function seriesPoints(self, si) {
 	const s = self.series[si];
 	const dia = ptDia(s.width, pxRatio);
-	let maxPts = self.bbox.width / s.points.space;
+	let maxPts = self.bbox.width / (s.points.space * pxRatio);
 	let idxs = self.series[0].idxs;
 	return idxs[1] - idxs[0] <= maxPts;
 }