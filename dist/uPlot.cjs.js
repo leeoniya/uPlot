leeoniya/uPlot

commit 936c9b242a8e006707b239a6386e7814e13534f7

don't set legend indicator styles to undefined. close #331.

dist/uPlot.cjs.js
@@ -1279,8 +1279,9 @@ function uPlot(opts, data, then) {
 		var label = placeTag("th", null, row);
 
 		var indic = placeDiv(LEGEND_MARKER, label);
-		indic.style.borderColor = s.width ? s.stroke : i > 0 && s.points.width ? s.points.stroke : null;
-		indic.style.backgroundColor = s.fill;
+		var borderColor = s.width ? s.stroke : i > 0 && s.points.width ? s.points.stroke : null;
+		indic.style.borderColor = borderColor || hexBlack;
+		indic.style.backgroundColor = s.fill || null;
 
 		var text = placeDiv(LEGEND_LABEL, label);
 		text.textContent = s.label;

dist/uPlot.esm.js
@@ -1282,8 +1282,9 @@ function uPlot(opts, data, then) {
 		let label = placeTag("th", null, row);
 
 		let indic = placeDiv(LEGEND_MARKER, label);
-		indic.style.borderColor = s.width ? s.stroke : i > 0 && s.points.width ? s.points.stroke : null;
-		indic.style.backgroundColor = s.fill;
+		let borderColor = s.width ? s.stroke : i > 0 && s.points.width ? s.points.stroke : null;
+		indic.style.borderColor = borderColor || hexBlack;
+		indic.style.backgroundColor = s.fill || null;
 
 		let text = placeDiv(LEGEND_LABEL, label);
 		text.textContent = s.label;

dist/uPlot.iife.js
@@ -1280,8 +1280,9 @@ var uPlot = (function () {
 			var label = placeTag("th", null, row);
 
 			var indic = placeDiv(LEGEND_MARKER, label);
-			indic.style.borderColor = s.width ? s.stroke : i > 0 && s.points.width ? s.points.stroke : null;
-			indic.style.backgroundColor = s.fill;
+			var borderColor = s.width ? s.stroke : i > 0 && s.points.width ? s.points.stroke : null;
+			indic.style.borderColor = borderColor || hexBlack;
+			indic.style.backgroundColor = s.fill || null;
 
 			var text = placeDiv(LEGEND_LABEL, label);
 			text.textContent = s.label;

src/uPlot.js
@@ -352,8 +352,9 @@ export default function uPlot(opts, data, then) {
 		let label = placeTag("th", null, row);
 
 		let indic = placeDiv(LEGEND_MARKER, label);
-		indic.style.borderColor = s.width ? s.stroke : i > 0 && s.points.width ? s.points.stroke : null;
-		indic.style.backgroundColor = s.fill;
+		let borderColor = s.width ? s.stroke : i > 0 && s.points.width ? s.points.stroke : null;
+		indic.style.borderColor = borderColor || hexBlack;
+		indic.style.backgroundColor = s.fill || null;
 
 		let text = placeDiv(LEGEND_LABEL, label);
 		text.textContent = s.label;

dist/uPlot.min.css
@@ -1,1 +1,1 @@
-.uplot, .uplot *, .uplot *::before, .uplot *::after {box-sizing: border-box;}.uplot {font-family: system-ui, -apple-system, "Segoe UI", Roboto, "Helvetica Neue", Arial, "Noto Sans", sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol", "Noto Color Emoji";line-height: 1.5;width: max-content;}.u-title {text-align: center;font-size: 18px;font-weight: bold;}.u-wrap {position: relative;user-select: none;}.u-over, .u-under {position: absolute;}.u-under {overflow: hidden;}.uplot canvas {display: block;position: relative;width: 100%;height: 100%;}.u-legend {font-size: 14px;margin: auto;text-align: center;}.u-inline {display: block;}.u-inline * {display: inline-block;}.u-inline tr {margin-right: 16px;}.u-legend th {font-weight: 600;}.u-legend th > * {vertical-align: middle;display: inline-block;}.u-legend .u-marker {width: 1em;height: 1em;margin-right: 4px;border: 2px solid transparent;}.u-inline.u-live th::after {content: ":";vertical-align: middle;}.u-inline:not(.u-live) .u-value {display: none;}.u-series > * {padding: 4px;}.u-series th {cursor: pointer;}.u-legend .u-off > * {opacity: 0.3;}.u-select {background: rgba(0,0,0,0.07);position: absolute;pointer-events: none;}.u-cursor-x, .u-cursor-y {position: absolute;left: 0;top: 0;pointer-events: none;will-change: transform;z-index: 100;}.u-cursor-x {height: 100%;border-right: 1px dashed #607D8B;}.u-cursor-y {width: 100%;border-bottom: 1px dashed #607D8B;}.u-cursor-pt {position: absolute;top: 0;left: 0;border-radius: 50%;filter: brightness(85%);pointer-events: none;will-change: transform;z-index: 100;}.u-select.u-off, .u-cursor-x.u-off, .u-cursor-y.u-off, .u-cursor-pt.u-off {display: none;}
+.uplot, .uplot *, .uplot *::before, .uplot *::after {box-sizing: border-box;}.uplot {font-family: system-ui, -apple-system, "Segoe UI", Roboto, "Helvetica Neue", Arial, "Noto Sans", sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol", "Noto Color Emoji";line-height: 1.5;width: max-content;}.u-title {text-align: center;font-size: 18px;font-weight: bold;}.u-wrap {position: relative;user-select: none;}.u-over, .u-under {position: absolute;}.u-under {overflow: hidden;}.uplot canvas {display: block;position: relative;width: 100%;height: 100%;}.u-legend {font-size: 14px;margin: auto;text-align: center;}.u-inline {display: block;}.u-inline * {display: inline-block;}.u-inline tr {margin-right: 16px;}.u-legend th {font-weight: 600;}.u-legend th > * {vertical-align: middle;display: inline-block;}.u-legend .u-marker {width: 1em;height: 1em;margin-right: 4px;border: 2px solid transparent;}.u-series:first-child .u-marker {display: none;}.u-inline.u-live th::after {content: ":";vertical-align: middle;}.u-inline:not(.u-live) .u-value {display: none;}.u-series > * {padding: 4px;}.u-series th {cursor: pointer;}.u-legend .u-off > * {opacity: 0.3;}.u-select {background: rgba(0,0,0,0.07);position: absolute;pointer-events: none;}.u-cursor-x, .u-cursor-y {position: absolute;left: 0;top: 0;pointer-events: none;will-change: transform;z-index: 100;}.u-cursor-x {height: 100%;border-right: 1px dashed #607D8B;}.u-cursor-y {width: 100%;border-bottom: 1px dashed #607D8B;}.u-cursor-pt {position: absolute;top: 0;left: 0;border-radius: 50%;filter: brightness(85%);pointer-events: none;will-change: transform;z-index: 100;}.u-select.u-off, .u-cursor-x.u-off, .u-cursor-y.u-off, .u-cursor-pt.u-off {display: none;}

src/uPlot.css
@@ -72,6 +72,10 @@
 	border: 2px solid transparent;
 }
 
+.u-series:first-child .u-marker {
+	display: none;
+}
+
 .u-inline.u-live th::after {
 	content: ":";
 	vertical-align: middle;

commit 5461983613aa0d542113822d492e7cf692b44a09

fall back to transparent instead of #000 for series.stroke (avoids screwing up fill-only series) fixes a regression in bar chart indicator borders caused by 936c9b242a8e006707b239a6386e7814e13534f7.

dist/uPlot.cjs.js
@@ -271,6 +271,7 @@ var RIGHT = "right";
 var firstChild = "firstChild";
 var createElement = "createElement";
 var hexBlack = "#000";
+var transparent = hexBlack + "0";
 var classList = "classList";
 
 var mousemove = "mousemove";
@@ -1285,8 +1286,7 @@ function uPlot(opts, data, then) {
 		var label = placeTag("th", null, row);
 
 		var indic = placeDiv(LEGEND_MARKER, label);
-		var borderColor = s.width ? s.stroke : i > 0 && s.points.width ? s.points.stroke : null;
-		indic.style.borderColor = borderColor || hexBlack;
+		indic.style.borderColor = s.width ? s.stroke : i > 0 && s.points.width ? s.points.stroke : null;
 		indic.style.backgroundColor = s.fill || null;
 
 		var text = placeDiv(LEGEND_LABEL, label);
@@ -1671,11 +1671,11 @@ function uPlot(opts, data, then) {
 	}
 
 	function setCtxStyle(stroke, width, dash, fill) {
-		ctx.strokeStyle = stroke || hexBlack;
+		ctx.strokeStyle = stroke || transparent;
 		ctx.lineWidth = width;
 		ctx.lineJoin = "round";
 		ctx.setLineDash(dash || []);
-		ctx.fillStyle = fill || hexBlack;
+		ctx.fillStyle = fill || transparent;
 	}
 
 	function setScales() {
@@ -1848,10 +1848,10 @@ function uPlot(opts, data, then) {
 		}
 
 		setCtxStyle(
-			p.stroke || hexBlack,
+			p.stroke,
 			width,
 			null,
-			p.fill || (isStroked ? "#fff" : s.stroke || hexBlack)
+			p.fill || (isStroked ? "#fff" : s.stroke)
 		);
 
 		ctx.fill(path);

dist/uPlot.esm.js
@@ -269,6 +269,7 @@ const RIGHT = "right";
 const firstChild = "firstChild";
 const createElement = "createElement";
 const hexBlack = "#000";
+const transparent = hexBlack + "0";
 const classList = "classList";
 
 const mousemove = "mousemove";
@@ -1288,8 +1289,7 @@ function uPlot(opts, data, then) {
 		let label = placeTag("th", null, row);
 
 		let indic = placeDiv(LEGEND_MARKER, label);
-		let borderColor = s.width ? s.stroke : i > 0 && s.points.width ? s.points.stroke : null;
-		indic.style.borderColor = borderColor || hexBlack;
+		indic.style.borderColor = s.width ? s.stroke : i > 0 && s.points.width ? s.points.stroke : null;
 		indic.style.backgroundColor = s.fill || null;
 
 		let text = placeDiv(LEGEND_LABEL, label);
@@ -1670,11 +1670,11 @@ function uPlot(opts, data, then) {
 	}
 
 	function setCtxStyle(stroke, width, dash, fill) {
-		ctx.strokeStyle = stroke || hexBlack;
+		ctx.strokeStyle = stroke || transparent;
 		ctx.lineWidth = width;
 		ctx.lineJoin = "round";
 		ctx.setLineDash(dash || []);
-		ctx.fillStyle = fill || hexBlack;
+		ctx.fillStyle = fill || transparent;
 	}
 
 	function setScales() {
@@ -1847,10 +1847,10 @@ function uPlot(opts, data, then) {
 		}
 
 		setCtxStyle(
-			p.stroke || hexBlack,
+			p.stroke,
 			width,
 			null,
-			p.fill || (isStroked ? "#fff" : s.stroke || hexBlack),
+			p.fill || (isStroked ? "#fff" : s.stroke),
 		);
 
 		ctx.fill(path);

dist/uPlot.iife.js
@@ -272,6 +272,7 @@ var uPlot = (function () {
 	var firstChild = "firstChild";
 	var createElement = "createElement";
 	var hexBlack = "#000";
+	var transparent = hexBlack + "0";
 	var classList = "classList";
 
 	var mousemove = "mousemove";
@@ -1286,8 +1287,7 @@ var uPlot = (function () {
 			var label = placeTag("th", null, row);
 
 			var indic = placeDiv(LEGEND_MARKER, label);
-			var borderColor = s.width ? s.stroke : i > 0 && s.points.width ? s.points.stroke : null;
-			indic.style.borderColor = borderColor || hexBlack;
+			indic.style.borderColor = s.width ? s.stroke : i > 0 && s.points.width ? s.points.stroke : null;
 			indic.style.backgroundColor = s.fill || null;
 
 			var text = placeDiv(LEGEND_LABEL, label);
@@ -1672,11 +1672,11 @@ var uPlot = (function () {
 		}
 
 		function setCtxStyle(stroke, width, dash, fill) {
-			ctx.strokeStyle = stroke || hexBlack;
+			ctx.strokeStyle = stroke || transparent;
 			ctx.lineWidth = width;
 			ctx.lineJoin = "round";
 			ctx.setLineDash(dash || []);
-			ctx.fillStyle = fill || hexBlack;
+			ctx.fillStyle = fill || transparent;
 		}
 
 		function setScales() {
@@ -1849,10 +1849,10 @@ var uPlot = (function () {
 			}
 
 			setCtxStyle(
-				p.stroke || hexBlack,
+				p.stroke,
 				width,
 				null,
-				p.fill || (isStroked ? "#fff" : s.stroke || hexBlack)
+				p.fill || (isStroked ? "#fff" : s.stroke)
 			);
 
 			ctx.fill(path);

src/strings.js
@@ -7,6 +7,7 @@ export const RIGHT = "right";
 export const firstChild = "firstChild";
 export const createElement = "createElement";
 export const hexBlack = "#000";
+export const transparent = hexBlack + "0";
 export const classList = "classList";
 
 export const mousemove = "mousemove";

src/uPlot.js
@@ -39,6 +39,7 @@ import {
 	LEFT,
 	RIGHT,
 	hexBlack,
+	transparent,
 	firstChild,
 
 	mousemove,
@@ -353,8 +354,7 @@ export default function uPlot(opts, data, then) {
 		let label = placeTag("th", null, row);
 
 		let indic = placeDiv(LEGEND_MARKER, label);
-		let borderColor = s.width ? s.stroke : i > 0 && s.points.width ? s.points.stroke : null;
-		indic.style.borderColor = borderColor || hexBlack;
+		indic.style.borderColor = s.width ? s.stroke : i > 0 && s.points.width ? s.points.stroke : null;
 		indic.style.backgroundColor = s.fill || null;
 
 		let text = placeDiv(LEGEND_LABEL, label);
@@ -736,11 +736,11 @@ export default function uPlot(opts, data, then) {
 	}
 
 	function setCtxStyle(stroke, width, dash, fill) {
-		ctx.strokeStyle = stroke || hexBlack;
+		ctx.strokeStyle = stroke || transparent;
 		ctx.lineWidth = width;
 		ctx.lineJoin = "round";
 		ctx.setLineDash(dash || []);
-		ctx.fillStyle = fill || hexBlack;
+		ctx.fillStyle = fill || transparent;
 	}
 
 	function setScales() {
@@ -913,10 +913,10 @@ export default function uPlot(opts, data, then) {
 		}
 
 		setCtxStyle(
-			p.stroke || hexBlack,
+			p.stroke,
 			width,
 			null,
-			p.fill || (isStroked ? "#fff" : s.stroke || hexBlack),
+			p.fill || (isStroked ? "#fff" : s.stroke),
 		);
 
 		ctx.fill(path);