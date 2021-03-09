4b34eddaf114f7ed86d1ba0286d9813951d1b4ad
Leon Sorokin <leeoniya@gmail.com>
2021-03-08 23:29:44 -0600

    1.6.6

--- dist/uPlot.cjs.js
+++ dist/uPlot.cjs.js
@@ -4,7 +4,7 @@
 *
 * uPlot.js (μPlot)
 * A small, fast chart for time series, lines, areas, ohlc & bars
-* https://github.com/leeoniya/uPlot (v1.6.5)
+* https://github.com/leeoniya/uPlot (v1.6.6)
 */
 
 'use strict';
