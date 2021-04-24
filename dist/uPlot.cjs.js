0d4ec6f239044034fad21cc7bc32febc0f374249
Martin André <martichou.andre@gmail.com>
2021-04-24 17:10:05 +0200

    don't update a hidden cursor when resizing, close #499.

--- dist/uPlot.cjs.js
+++ dist/uPlot.cjs.js
@@ -2489,7 +2489,7 @@ function uPlot(opts, data, then) {
 
 		shouldConvergeSize = true;
 		shouldSetSize = true;
-		shouldSetCursor = true;
+		shouldSetCursor = cursor.left >= 0;
 		shouldSetLegend = true;
 		commit();
 	}
