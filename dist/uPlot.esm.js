0ccd572074199f013733a89c4d756b7502a759c6
Leon Sorokin <leeoniya@gmail.com>
2021-05-01 17:18:00 -0500

    1.6.9

--- dist/uPlot.esm.js
+++ dist/uPlot.esm.js
@@ -4,7 +4,7 @@
 *
 * uPlot.js (μPlot)
 * A small, fast chart for time series, lines, areas, ohlc & bars
-* https://github.com/leeoniya/uPlot (v1.6.8)
+* https://github.com/leeoniya/uPlot (v1.6.9)
 */
 
 const FEAT_TIME          = true;
