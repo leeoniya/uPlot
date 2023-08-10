leeoniya/uPlot

commit 04207a01cd6c6d7cc7764914cb760d445ab1f7d6

more robust log10 scale tick filtering. close #826.

dist/uPlot.cjs.js
@@ -1410,7 +1410,7 @@ function log10AxisValsFilt(self, splits, axisIdx, foundSpace, foundIncr) {
 		RE_1
 	);
 
-	return splits.map(v => ((sc.distr == 4 && v == 0) || re.test(v)) ? v : null);
+	return splits.map(v => ((sc.distr == 4 && v == 0) || re.test(v.toExponential()[0])) ? v : null);
 }
 
 function numSeriesVal(self, val, seriesIdx, dataIdx) {

dist/uPlot.esm.js
@@ -1408,7 +1408,7 @@ function log10AxisValsFilt(self, splits, axisIdx, foundSpace, foundIncr) {
 		RE_1
 	);
 
-	return splits.map(v => ((sc.distr == 4 && v == 0) || re.test(v)) ? v : null);
+	return splits.map(v => ((sc.distr == 4 && v == 0) || re.test(v.toExponential()[0])) ? v : null);
 }
 
 function numSeriesVal(self, val, seriesIdx, dataIdx) {

dist/uPlot.iife.js
@@ -1411,7 +1411,7 @@ var uPlot = (function () {
 			RE_1
 		);
 
-		return splits.map(v => ((sc.distr == 4 && v == 0) || re.test(v)) ? v : null);
+		return splits.map(v => ((sc.distr == 4 && v == 0) || re.test(v.toExponential()[0])) ? v : null);
 	}
 
 	function numSeriesVal(self, val, seriesIdx, dataIdx) {

commit b89dc1add990abad73858f5bae6205b573fd1f5d

fix neg tick filtering with asinh scales (regressed by 04207a0)

dist/uPlot.cjs.js
@@ -1498,7 +1498,7 @@ const RE_12357 = /[12357]/;
 const RE_125   = /[125]/;
 const RE_1     = /1/;
 
-const _filt = (splits, distr, re, keepMod) => splits.map((v, i) => ((distr == 4 && v == 0) || i % keepMod == 0 && re.test(v.toExponential()[0])) ? v : null);
+const _filt = (splits, distr, re, keepMod) => splits.map((v, i) => ((distr == 4 && v == 0) || i % keepMod == 0 && re.test(v.toExponential()[v < 0 ? 1 : 0])) ? v : null);
 
 function log10AxisValsFilt(self, splits, axisIdx, foundSpace, foundIncr) {
 	let axis = self.axes[axisIdx];

dist/uPlot.esm.js
@@ -1496,7 +1496,7 @@ const RE_12357 = /[12357]/;
 const RE_125   = /[125]/;
 const RE_1     = /1/;
 
-const _filt = (splits, distr, re, keepMod) => splits.map((v, i) => ((distr == 4 && v == 0) || i % keepMod == 0 && re.test(v.toExponential()[0])) ? v : null);
+const _filt = (splits, distr, re, keepMod) => splits.map((v, i) => ((distr == 4 && v == 0) || i % keepMod == 0 && re.test(v.toExponential()[v < 0 ? 1 : 0])) ? v : null);
 
 function log10AxisValsFilt(self, splits, axisIdx, foundSpace, foundIncr) {
 	let axis = self.axes[axisIdx];

dist/uPlot.iife.js
@@ -1499,7 +1499,7 @@ var uPlot = (function () {
 	const RE_125   = /[125]/;
 	const RE_1     = /1/;
 
-	const _filt = (splits, distr, re, keepMod) => splits.map((v, i) => ((distr == 4 && v == 0) || i % keepMod == 0 && re.test(v.toExponential()[0])) ? v : null);
+	const _filt = (splits, distr, re, keepMod) => splits.map((v, i) => ((distr == 4 && v == 0) || i % keepMod == 0 && re.test(v.toExponential()[v < 0 ? 1 : 0])) ? v : null);
 
 	function log10AxisValsFilt(self, splits, axisIdx, foundSpace, foundIncr) {
 		let axis = self.axes[axisIdx];

src/opts.js
@@ -601,7 +601,7 @@ const RE_12357 = /[12357]/;
 const RE_125   = /[125]/;
 const RE_1     = /1/;
 
-const _filt = (splits, distr, re, keepMod) => splits.map((v, i) => ((distr == 4 && v == 0) || i % keepMod == 0 && re.test(v.toExponential()[0])) ? v : null);
+const _filt = (splits, distr, re, keepMod) => splits.map((v, i) => ((distr == 4 && v == 0) || i % keepMod == 0 && re.test(v.toExponential()[v < 0 ? 1 : 0])) ? v : null);
 
 export function log10AxisValsFilt(self, splits, axisIdx, foundSpace, foundIncr) {
 	let axis = self.axes[axisIdx];