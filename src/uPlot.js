2eef7f31e20b5044e3f2ffb850a0ccdea9ac7a9a
progitect <153317295+progitect@users.noreply.github.com>
2023-12-11 11:24:43 +0100

    Fix null error in syncLegend-method

--- src/uPlot.js
+++ src/uPlot.js
@@ -2394,8 +2394,10 @@ export default function uPlot(opts, data, then) {
 
 				let j = 0;
 
-				for (let k in vals)
-					legendCells[i][j++].firstChild.nodeValue = vals[k];
+				for (let k in vals) {
+          if (legendCells[i][j].firstChild?.nodeValue)
+            legendCells[i][j++].firstChild.nodeValue = vals[k];
+        }
 			}
 		}
 	}
