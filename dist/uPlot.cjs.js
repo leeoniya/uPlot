leeoniya/uPlot

commit 00af5e854b2c11725ac0b87402c994e4bceaf86c

unify bar width and stroke rendering strategies for bars.disp

demos/latency-heatmap.html
@@ -674,7 +674,7 @@
 			const opts3 = {
 				width: 1800,
 				height: 600,
-				title: "Latency Histogram",
+				title: "Latency Histogram (align: 1, gap: 0, stroke: 2, border collapse)",
 				scales: {
 					x: {
 						time: false,
@@ -714,7 +714,7 @@
 						points: {
 							show: false,
 						},
-						fill: "rgba(255,0,0,0.2)",
+						fill: "rgba(255,0,0,0.4)",
 						stroke: "rgba(255,0,0,1)",
 						width: 2,
 					},
@@ -752,6 +752,90 @@
 
 				u3.setData(hist);
 			};
+
+			{
+				let bars = uPlot.paths.bars({
+					align: 0,
+					size: [1, Infinity],
+					gap: 2,
+
+					disp: {
+						x0: {
+							unit: 1,
+							values: (u, seriesIdx, idx0, idx1) => {
+								let xData = u.data[0];
+								let size = xData[1] - xData[0];
+								let halfWid = size / 2;
+
+								return xData.map(v => v + halfWid);
+							},
+						},
+						size: {
+							unit: 1,
+							values: (u, seriesIdx, idx0, idx1) => {
+								let xData = u.data[0];
+								let size = xData[1] - xData[0];
+								return Array(xData.length).fill(size);
+							}
+						}
+					}
+				});
+
+				const opts3 = {
+					width: 1800,
+					height: 600,
+					title: "Latency Histogram (align: 0 (+ disp-shift), gap: 2, stroke: 1)",
+					scales: {
+						x: {
+							time: false,
+							auto: false,
+							dir: 1,
+							range: (u) => [
+								u.data[0][0],
+								u.data[0][u.data[0].length - 1] + bucketIncr,
+							]
+						}
+					},
+					axes: [
+						{
+							grid: {
+								show: false,
+							},
+							incrs: () => [0,1,2,3,4,5,6,7,8,9,10].map(mult => mult * bucketIncr),
+						//	space: 0,
+							splits: (u, axisIdx, scaleMin, scaleMax, foundIncr, foundSpace) => {
+								let minSpace = u.axes[axisIdx]._space;
+								let bucketWidth = u.valToPos(u.data[0][0] + bucketIncr, 'x') - u.valToPos(u.data[0][0], 'x');
+
+								let firstSplit = u.data[0][0];
+								let lastSplit = u.data[0][u.data[0].length - 1] + bucketIncr;
+
+								let splits = [];
+								let skip = Math.ceil(minSpace / bucketWidth);
+
+								for (let i = 0, s = firstSplit; s <= lastSplit; i++, s += bucketIncr)
+									!(i % skip) && splits.push(s);
+
+								return splits;
+							},
+						}
+					],
+					series: [
+						{},
+						{
+							paths: bars,
+							fill: "rgba(255,0,0,0.4)",
+							stroke: "rgba(255,0,0,1)",
+							points: {
+								show: false,
+							},
+							width: 1,
+						},
+					],
+				};
+
+				new uPlot(opts3, hist, document.body);
+			}
 		</script>
 	</body>
 </html>

dist/uPlot.cjs.js
@@ -2308,6 +2308,32 @@ function stepped(opts) {
 	};
 }
 
+function findColWidth(dataX, dataY, valToPosX, scaleX, xDim, xOff, colWid = inf) {
+	if (dataX.length > 1) {
+		// prior index with non-undefined y data
+		let prevIdx = null;
+
+		// scan full dataset for smallest adjacent delta
+		// will not work properly for non-linear x scales, since does not do expensive valToPosX calcs till end
+		for (let i = 0, minDelta = Infinity; i < dataX.length; i++) {
+			if (dataY[i] !== undefined) {
+				if (prevIdx != null) {
+					let delta = abs(dataX[i] - dataX[prevIdx]);
+
+					if (delta < minDelta) {
+						minDelta = delta;
+						colWid = abs(valToPosX(dataX[i], scaleX, xDim, xOff) - valToPosX(dataX[prevIdx], scaleX, xDim, xOff));
+					}
+				}
+
+				prevIdx = i;
+			}
+		}
+	}
+
+	return colWid;
+}
+
 function bars(opts) {
 	opts = opts || EMPTY_OBJ;
 	const size = ifNull(opts.size, [0.6, inf, 1]);
@@ -2364,7 +2390,7 @@ function bars(opts) {
 			let fillToY = pxRound(valToPosY(fillTo, scaleY, yDim, yOff));
 
 			// barWid is to center of stroke
-			let xShift, barWid;
+			let xShift, barWid, fullGap, colWid = xDim;
 
 			let strokeWidth = pxRound(series.width * pxRatio);
 
@@ -2411,63 +2437,38 @@ function bars(opts) {
 				else
 					barWid = valToPosX(sizes[0], scaleX, xDim, xOff) - valToPosX(0, scaleX, xDim, xOff); // assumes linear scale (delta from 0)
 
-				if (strokeWidth >= barWid / 2)
-					strokeWidth = 0;
+				colWid = findColWidth(dataX, dataY, valToPosX, scaleX, xDim, xOff, colWid);
 
-				// for small gaps, disable pixel snapping since gap inconsistencies become noticible and annoying
-			//	if (gapWid + extraGap < 5)
-			//		pxRound = retArg0;
-
-				barWid = pxRound(clamp(barWid - strokeWidth, minWidth, maxWidth)); // TODO: extraGap?
-
-				xShift = (_dirX == 1 ? -strokeWidth / 2 : barWid + strokeWidth / 2);
+				let gapWid = colWid - barWid;
+				fullGap = gapWid + extraGap;
 			}
 			else {
-				let colWid = xDim;
-
-				if (dataX.length > 1) {
-					// prior index with non-undefined y data
-					let prevIdx = null;
-
-					// scan full dataset for smallest adjacent delta
-					// will not work properly for non-linear x scales, since does not do expensive valToPosX calcs till end
-					for (let i = 0, minDelta = Infinity; i < dataX.length; i++) {
-						if (dataY[i] !== undefined) {
-							if (prevIdx != null) {
-								let delta = abs(dataX[i] - dataX[prevIdx]);
-
-								if (delta < minDelta) {
-									minDelta = delta;
-									colWid = abs(valToPosX(dataX[i], scaleX, xDim, xOff) - valToPosX(dataX[prevIdx], scaleX, xDim, xOff));
-								}
-							}
-
-							prevIdx = i;
-						}
-					}
-				}
+				colWid = findColWidth(dataX, dataY, valToPosX, scaleX, xDim, xOff, colWid);
 
 				let gapWid = colWid * gapFactor;
 
-				let fullGap = gapWid + extraGap;
-
+				fullGap = gapWid + extraGap;
 				barWid = colWid - fullGap;
-
-				if (strokeWidth >= barWid / 2)
-					strokeWidth = 0;
-
-				// for small gaps, disable pixel snapping since gap inconsistencies become noticible and annoying
-				if (fullGap < 5)
-					pxRound = retArg0;
-
-				let insetStroke = fullGap > 0;
-
-				let rawBarWid = colWid - fullGap - (insetStroke ? strokeWidth : 0);
-
-				barWid = pxRound(clamp(rawBarWid, minWidth, maxWidth));
-
-				xShift = (align == 0 ? barWid / 2 : align == _dirX ? 0 : barWid) - align * _dirX * ((align == 0 ? extraGap / 2 : 0) + (insetStroke ? strokeWidth / 2 : 0));
 			}
+
+			if (fullGap < 1)
+				fullGap = 0;
+
+			if (strokeWidth >= barWid / 2)
+				strokeWidth = 0;
+
+			// for small gaps, disable pixel snapping since gap inconsistencies become noticible and annoying
+			if (fullGap < 5)
+				pxRound = retArg0;
+
+			let insetStroke = fullGap > 0;
+
+			let rawBarWid = colWid - fullGap - (insetStroke ? strokeWidth : 0);
+
+			barWid = pxRound(clamp(rawBarWid, minWidth, maxWidth));
+
+			xShift = (align == 0 ? barWid / 2 : align == _dirX ? 0 : barWid) - align * _dirX * ((align == 0 ? extraGap / 2 : 0) + (insetStroke ? strokeWidth / 2 : 0));
+
 
 			const _paths = {stroke: null, fill: null, clip: null, band: null, gaps: null, flags: 0};  // disp, geom
 

dist/uPlot.esm.js
@@ -2306,6 +2306,32 @@ function stepped(opts) {
 	};
 }
 
+function findColWidth(dataX, dataY, valToPosX, scaleX, xDim, xOff, colWid = inf) {
+	if (dataX.length > 1) {
+		// prior index with non-undefined y data
+		let prevIdx = null;
+
+		// scan full dataset for smallest adjacent delta
+		// will not work properly for non-linear x scales, since does not do expensive valToPosX calcs till end
+		for (let i = 0, minDelta = Infinity; i < dataX.length; i++) {
+			if (dataY[i] !== undefined) {
+				if (prevIdx != null) {
+					let delta = abs(dataX[i] - dataX[prevIdx]);
+
+					if (delta < minDelta) {
+						minDelta = delta;
+						colWid = abs(valToPosX(dataX[i], scaleX, xDim, xOff) - valToPosX(dataX[prevIdx], scaleX, xDim, xOff));
+					}
+				}
+
+				prevIdx = i;
+			}
+		}
+	}
+
+	return colWid;
+}
+
 function bars(opts) {
 	opts = opts || EMPTY_OBJ;
 	const size = ifNull(opts.size, [0.6, inf, 1]);
@@ -2362,7 +2388,7 @@ function bars(opts) {
 			let fillToY = pxRound(valToPosY(fillTo, scaleY, yDim, yOff));
 
 			// barWid is to center of stroke
-			let xShift, barWid;
+			let xShift, barWid, fullGap, colWid = xDim;
 
 			let strokeWidth = pxRound(series.width * pxRatio);
 
@@ -2409,63 +2435,38 @@ function bars(opts) {
 				else
 					barWid = valToPosX(sizes[0], scaleX, xDim, xOff) - valToPosX(0, scaleX, xDim, xOff); // assumes linear scale (delta from 0)
 
-				if (strokeWidth >= barWid / 2)
-					strokeWidth = 0;
+				colWid = findColWidth(dataX, dataY, valToPosX, scaleX, xDim, xOff, colWid);
 
-				// for small gaps, disable pixel snapping since gap inconsistencies become noticible and annoying
-			//	if (gapWid + extraGap < 5)
-			//		pxRound = retArg0;
-
-				barWid = pxRound(clamp(barWid - strokeWidth, minWidth, maxWidth)); // TODO: extraGap?
-
-				xShift = (_dirX == 1 ? -strokeWidth / 2 : barWid + strokeWidth / 2);
+				let gapWid = colWid - barWid;
+				fullGap = gapWid + extraGap;
 			}
 			else {
-				let colWid = xDim;
-
-				if (dataX.length > 1) {
-					// prior index with non-undefined y data
-					let prevIdx = null;
-
-					// scan full dataset for smallest adjacent delta
-					// will not work properly for non-linear x scales, since does not do expensive valToPosX calcs till end
-					for (let i = 0, minDelta = Infinity; i < dataX.length; i++) {
-						if (dataY[i] !== undefined) {
-							if (prevIdx != null) {
-								let delta = abs(dataX[i] - dataX[prevIdx]);
-
-								if (delta < minDelta) {
-									minDelta = delta;
-									colWid = abs(valToPosX(dataX[i], scaleX, xDim, xOff) - valToPosX(dataX[prevIdx], scaleX, xDim, xOff));
-								}
-							}
-
-							prevIdx = i;
-						}
-					}
-				}
+				colWid = findColWidth(dataX, dataY, valToPosX, scaleX, xDim, xOff, colWid);
 
 				let gapWid = colWid * gapFactor;
 
-				let fullGap = gapWid + extraGap;
-
+				fullGap = gapWid + extraGap;
 				barWid = colWid - fullGap;
-
-				if (strokeWidth >= barWid / 2)
-					strokeWidth = 0;
-
-				// for small gaps, disable pixel snapping since gap inconsistencies become noticible and annoying
-				if (fullGap < 5)
-					pxRound = retArg0;
-
-				let insetStroke = fullGap > 0;
-
-				let rawBarWid = colWid - fullGap - (insetStroke ? strokeWidth : 0);
-
-				barWid = pxRound(clamp(rawBarWid, minWidth, maxWidth));
-
-				xShift = (align == 0 ? barWid / 2 : align == _dirX ? 0 : barWid) - align * _dirX * ((align == 0 ? extraGap / 2 : 0) + (insetStroke ? strokeWidth / 2 : 0));
 			}
+
+			if (fullGap < 1)
+				fullGap = 0;
+
+			if (strokeWidth >= barWid / 2)
+				strokeWidth = 0;
+
+			// for small gaps, disable pixel snapping since gap inconsistencies become noticible and annoying
+			if (fullGap < 5)
+				pxRound = retArg0;
+
+			let insetStroke = fullGap > 0;
+
+			let rawBarWid = colWid - fullGap - (insetStroke ? strokeWidth : 0);
+
+			barWid = pxRound(clamp(rawBarWid, minWidth, maxWidth));
+
+			xShift = (align == 0 ? barWid / 2 : align == _dirX ? 0 : barWid) - align * _dirX * ((align == 0 ? extraGap / 2 : 0) + (insetStroke ? strokeWidth / 2 : 0));
+
 
 			const _paths = {stroke: null, fill: null, clip: null, band: null, gaps: null, flags: 0};  // disp, geom
 

dist/uPlot.iife.js
@@ -2309,6 +2309,32 @@ var uPlot = (function () {
 		};
 	}
 
+	function findColWidth(dataX, dataY, valToPosX, scaleX, xDim, xOff, colWid = inf) {
+		if (dataX.length > 1) {
+			// prior index with non-undefined y data
+			let prevIdx = null;
+
+			// scan full dataset for smallest adjacent delta
+			// will not work properly for non-linear x scales, since does not do expensive valToPosX calcs till end
+			for (let i = 0, minDelta = Infinity; i < dataX.length; i++) {
+				if (dataY[i] !== undefined) {
+					if (prevIdx != null) {
+						let delta = abs(dataX[i] - dataX[prevIdx]);
+
+						if (delta < minDelta) {
+							minDelta = delta;
+							colWid = abs(valToPosX(dataX[i], scaleX, xDim, xOff) - valToPosX(dataX[prevIdx], scaleX, xDim, xOff));
+						}
+					}
+
+					prevIdx = i;
+				}
+			}
+		}
+
+		return colWid;
+	}
+
 	function bars(opts) {
 		opts = opts || EMPTY_OBJ;
 		const size = ifNull(opts.size, [0.6, inf, 1]);
@@ -2365,7 +2391,7 @@ var uPlot = (function () {
 				let fillToY = pxRound(valToPosY(fillTo, scaleY, yDim, yOff));
 
 				// barWid is to center of stroke
-				let xShift, barWid;
+				let xShift, barWid, fullGap, colWid = xDim;
 
 				let strokeWidth = pxRound(series.width * pxRatio);
 
@@ -2412,63 +2438,38 @@ var uPlot = (function () {
 					else
 						barWid = valToPosX(sizes[0], scaleX, xDim, xOff) - valToPosX(0, scaleX, xDim, xOff); // assumes linear scale (delta from 0)
 
-					if (strokeWidth >= barWid / 2)
-						strokeWidth = 0;
+					colWid = findColWidth(dataX, dataY, valToPosX, scaleX, xDim, xOff, colWid);
 
-					// for small gaps, disable pixel snapping since gap inconsistencies become noticible and annoying
-				//	if (gapWid + extraGap < 5)
-				//		pxRound = retArg0;
-
-					barWid = pxRound(clamp(barWid - strokeWidth, minWidth, maxWidth)); // TODO: extraGap?
-
-					xShift = (_dirX == 1 ? -strokeWidth / 2 : barWid + strokeWidth / 2);
+					let gapWid = colWid - barWid;
+					fullGap = gapWid + extraGap;
 				}
 				else {
-					let colWid = xDim;
-
-					if (dataX.length > 1) {
-						// prior index with non-undefined y data
-						let prevIdx = null;
-
-						// scan full dataset for smallest adjacent delta
-						// will not work properly for non-linear x scales, since does not do expensive valToPosX calcs till end
-						for (let i = 0, minDelta = Infinity; i < dataX.length; i++) {
-							if (dataY[i] !== undefined) {
-								if (prevIdx != null) {
-									let delta = abs(dataX[i] - dataX[prevIdx]);
-
-									if (delta < minDelta) {
-										minDelta = delta;
-										colWid = abs(valToPosX(dataX[i], scaleX, xDim, xOff) - valToPosX(dataX[prevIdx], scaleX, xDim, xOff));
-									}
-								}
-
-								prevIdx = i;
-							}
-						}
-					}
+					colWid = findColWidth(dataX, dataY, valToPosX, scaleX, xDim, xOff, colWid);
 
 					let gapWid = colWid * gapFactor;
 
-					let fullGap = gapWid + extraGap;
-
+					fullGap = gapWid + extraGap;
 					barWid = colWid - fullGap;
-
-					if (strokeWidth >= barWid / 2)
-						strokeWidth = 0;
-
-					// for small gaps, disable pixel snapping since gap inconsistencies become noticible and annoying
-					if (fullGap < 5)
-						pxRound = retArg0;
-
-					let insetStroke = fullGap > 0;
-
-					let rawBarWid = colWid - fullGap - (insetStroke ? strokeWidth : 0);
-
-					barWid = pxRound(clamp(rawBarWid, minWidth, maxWidth));
-
-					xShift = (align == 0 ? barWid / 2 : align == _dirX ? 0 : barWid) - align * _dirX * ((align == 0 ? extraGap / 2 : 0) + (insetStroke ? strokeWidth / 2 : 0));
 				}
+
+				if (fullGap < 1)
+					fullGap = 0;
+
+				if (strokeWidth >= barWid / 2)
+					strokeWidth = 0;
+
+				// for small gaps, disable pixel snapping since gap inconsistencies become noticible and annoying
+				if (fullGap < 5)
+					pxRound = retArg0;
+
+				let insetStroke = fullGap > 0;
+
+				let rawBarWid = colWid - fullGap - (insetStroke ? strokeWidth : 0);
+
+				barWid = pxRound(clamp(rawBarWid, minWidth, maxWidth));
+
+				xShift = (align == 0 ? barWid / 2 : align == _dirX ? 0 : barWid) - align * _dirX * ((align == 0 ? extraGap / 2 : 0) + (insetStroke ? strokeWidth / 2 : 0));
+
 
 				const _paths = {stroke: null, fill: null, clip: null, band: null, gaps: null, flags: 0};  // disp, geom
 

src/paths/bars.js
@@ -2,6 +2,32 @@ import { abs, floor, min, max, inf, ifNull, EMPTY_OBJ, fnOrSelf, clamp, retArg0,
 import { orient, rectV, rectH } from './utils';
 import { pxRatio } from '../dom';
 
+function findColWidth(dataX, dataY, valToPosX, scaleX, xDim, xOff, colWid = inf) {
+	if (dataX.length > 1) {
+		// prior index with non-undefined y data
+		let prevIdx = null;
+
+		// scan full dataset for smallest adjacent delta
+		// will not work properly for non-linear x scales, since does not do expensive valToPosX calcs till end
+		for (let i = 0, minDelta = Infinity; i < dataX.length; i++) {
+			if (dataY[i] !== undefined) {
+				if (prevIdx != null) {
+					let delta = abs(dataX[i] - dataX[prevIdx]);
+
+					if (delta < minDelta) {
+						minDelta = delta;
+						colWid = abs(valToPosX(dataX[i], scaleX, xDim, xOff) - valToPosX(dataX[prevIdx], scaleX, xDim, xOff));
+					}
+				}
+
+				prevIdx = i;
+			}
+		}
+	}
+
+	return colWid;
+}
+
 export function bars(opts) {
 	opts = opts || EMPTY_OBJ;
 	const size = ifNull(opts.size, [0.6, inf, 1]);
@@ -58,7 +84,7 @@ export function bars(opts) {
 			let fillToY = pxRound(valToPosY(fillTo, scaleY, yDim, yOff))
 
 			// barWid is to center of stroke
-			let xShift, barWid;
+			let xShift, barWid, fullGap, colWid = xDim;
 
 			let strokeWidth = pxRound(series.width * pxRatio);
 
@@ -105,63 +131,38 @@ export function bars(opts) {
 				else
 					barWid = valToPosX(sizes[0], scaleX, xDim, xOff) - valToPosX(0, scaleX, xDim, xOff); // assumes linear scale (delta from 0)
 
-				if (strokeWidth >= barWid / 2)
-					strokeWidth = 0;
+				colWid = findColWidth(dataX, dataY, valToPosX, scaleX, xDim, xOff, colWid);
 
-				// for small gaps, disable pixel snapping since gap inconsistencies become noticible and annoying
-			//	if (gapWid + extraGap < 5)
-			//		pxRound = retArg0;
-
-				barWid = pxRound(clamp(barWid - strokeWidth, minWidth, maxWidth)); // TODO: extraGap?
-
-				xShift = (_dirX == 1 ? -strokeWidth / 2 : barWid + strokeWidth / 2);
+				let gapWid = colWid - barWid;
+				fullGap = gapWid + extraGap;
 			}
 			else {
-				let colWid = xDim;
-
-				if (dataX.length > 1) {
-					// prior index with non-undefined y data
-					let prevIdx = null;
-
-					// scan full dataset for smallest adjacent delta
-					// will not work properly for non-linear x scales, since does not do expensive valToPosX calcs till end
-					for (let i = 0, minDelta = Infinity; i < dataX.length; i++) {
-						if (dataY[i] !== undefined) {
-							if (prevIdx != null) {
-								let delta = abs(dataX[i] - dataX[prevIdx]);
-
-								if (delta < minDelta) {
-									minDelta = delta;
-									colWid = abs(valToPosX(dataX[i], scaleX, xDim, xOff) - valToPosX(dataX[prevIdx], scaleX, xDim, xOff));
-								}
-							}
-
-							prevIdx = i;
-						}
-					}
-				}
+				colWid = findColWidth(dataX, dataY, valToPosX, scaleX, xDim, xOff, colWid);
 
 				let gapWid = colWid * gapFactor;
 
-				let fullGap = gapWid + extraGap;
-
+				fullGap = gapWid + extraGap;
 				barWid = colWid - fullGap;
-
-				if (strokeWidth >= barWid / 2)
-					strokeWidth = 0;
-
-				// for small gaps, disable pixel snapping since gap inconsistencies become noticible and annoying
-				if (fullGap < 5)
-					pxRound = retArg0;
-
-				let insetStroke = fullGap > 0;
-
-				let rawBarWid = colWid - fullGap - (insetStroke ? strokeWidth : 0);
-
-				barWid = pxRound(clamp(rawBarWid, minWidth, maxWidth));
-
-				xShift = (align == 0 ? barWid / 2 : align == _dirX ? 0 : barWid) - align * _dirX * ((align == 0 ? extraGap / 2 : 0) + (insetStroke ? strokeWidth / 2 : 0));
 			}
+
+			if (fullGap < 1)
+				fullGap = 0;
+
+			if (strokeWidth >= barWid / 2)
+				strokeWidth = 0;
+
+			// for small gaps, disable pixel snapping since gap inconsistencies become noticible and annoying
+			if (fullGap < 5)
+				pxRound = retArg0;
+
+			let insetStroke = fullGap > 0;
+
+			let rawBarWid = colWid - fullGap - (insetStroke ? strokeWidth : 0);
+
+			barWid = pxRound(clamp(rawBarWid, minWidth, maxWidth));
+
+			xShift = (align == 0 ? barWid / 2 : align == _dirX ? 0 : barWid) - align * _dirX * ((align == 0 ? extraGap / 2 : 0) + (insetStroke ? strokeWidth / 2 : 0));
+
 
 			const _paths = {stroke: null, fill: null, clip: null, band: null, gaps: null, flags: 0};  // disp, geom
 

commit fb28b8507436c84453dede5e6ff59570f354e4ea

fix disp alignment regression from 00af5e854b2c11725ac0b87402c994e4bceaf86c should default to 1 when x0 is supplied

demos/latency-heatmap.html
@@ -754,21 +754,20 @@
 			};
 
 			{
+				let gap = 3;
 				let bars = uPlot.paths.bars({
-					align: 0,
 					size: [1, Infinity],
-					gap: 2,
+					gap,
 
 					disp: {
 						x0: {
 							unit: 1,
 							values: (u, seriesIdx, idx0, idx1) => {
 								let xData = u.data[0];
-								let size = xData[1] - xData[0];
-								let halfWid = size / 2;
-
-								return xData.map(v => v + halfWid);
-							},
+								let x0Val = xData[0];
+								let gapDelta = u.posToVal(gap, 'x') - x0Val;
+								return xData.map(v => v + gapDelta / 2);
+							}
 						},
 						size: {
 							unit: 1,
@@ -784,7 +783,7 @@
 				const opts3 = {
 					width: 1800,
 					height: 600,
-					title: "Latency Histogram (align: 0 (+ disp-shift), gap: 2, stroke: 1)",
+					title: "Latency Histogram (align: 1, gap: 2, stroke: 1, disp-gap-shift)",
 					scales: {
 						x: {
 							time: false,

dist/uPlot.cjs.js
@@ -2363,6 +2363,7 @@ function bars(opts) {
 	return (u, seriesIdx, idx0, idx1) => {
 		return orient(u, seriesIdx, (series, dataX, dataY, scaleX, scaleY, valToPosX, valToPosY, xOff, yOff, xDim, yDim) => {
 			let pxRound = series.pxRound;
+			let _align = align;
 
 			let extraGap = _extraGap * pxRatio;
 			let maxWidth = _maxWidth * pxRatio;
@@ -2426,6 +2427,7 @@ function bars(opts) {
 			let { x0, size } = disp;
 
 			if (x0 != null && size != null) {
+				_align = 1;
 				dataX = x0.values(u, seriesIdx, idx0, idx1);
 
 				if (x0.unit == 2)
@@ -2469,7 +2471,7 @@ function bars(opts) {
 
 			barWid = pxRound(clamp(rawBarWid, minWidth, maxWidth));
 
-			xShift = (align == 0 ? barWid / 2 : align == _dirX ? 0 : barWid) - align * _dirX * ((align == 0 ? extraGap / 2 : 0) + (insetStroke ? strokeWidth / 2 : 0));
+			xShift = (_align == 0 ? barWid / 2 : _align == _dirX ? 0 : barWid) - _align * _dirX * ((_align == 0 ? extraGap / 2 : 0) + (insetStroke ? strokeWidth / 2 : 0));
 
 
 			const _paths = {stroke: null, fill: null, clip: null, band: null, gaps: null, flags: 0};  // disp, geom

dist/uPlot.esm.js
@@ -2361,6 +2361,7 @@ function bars(opts) {
 	return (u, seriesIdx, idx0, idx1) => {
 		return orient(u, seriesIdx, (series, dataX, dataY, scaleX, scaleY, valToPosX, valToPosY, xOff, yOff, xDim, yDim) => {
 			let pxRound = series.pxRound;
+			let _align = align;
 
 			let extraGap = _extraGap * pxRatio;
 			let maxWidth = _maxWidth * pxRatio;
@@ -2424,6 +2425,7 @@ function bars(opts) {
 			let { x0, size } = disp;
 
 			if (x0 != null && size != null) {
+				_align = 1;
 				dataX = x0.values(u, seriesIdx, idx0, idx1);
 
 				if (x0.unit == 2)
@@ -2467,7 +2469,7 @@ function bars(opts) {
 
 			barWid = pxRound(clamp(rawBarWid, minWidth, maxWidth));
 
-			xShift = (align == 0 ? barWid / 2 : align == _dirX ? 0 : barWid) - align * _dirX * ((align == 0 ? extraGap / 2 : 0) + (insetStroke ? strokeWidth / 2 : 0));
+			xShift = (_align == 0 ? barWid / 2 : _align == _dirX ? 0 : barWid) - _align * _dirX * ((_align == 0 ? extraGap / 2 : 0) + (insetStroke ? strokeWidth / 2 : 0));
 
 
 			const _paths = {stroke: null, fill: null, clip: null, band: null, gaps: null, flags: 0};  // disp, geom

dist/uPlot.iife.js
@@ -2364,6 +2364,7 @@ var uPlot = (function () {
 		return (u, seriesIdx, idx0, idx1) => {
 			return orient(u, seriesIdx, (series, dataX, dataY, scaleX, scaleY, valToPosX, valToPosY, xOff, yOff, xDim, yDim) => {
 				let pxRound = series.pxRound;
+				let _align = align;
 
 				let extraGap = _extraGap * pxRatio;
 				let maxWidth = _maxWidth * pxRatio;
@@ -2427,6 +2428,7 @@ var uPlot = (function () {
 				let { x0, size } = disp;
 
 				if (x0 != null && size != null) {
+					_align = 1;
 					dataX = x0.values(u, seriesIdx, idx0, idx1);
 
 					if (x0.unit == 2)
@@ -2470,7 +2472,7 @@ var uPlot = (function () {
 
 				barWid = pxRound(clamp(rawBarWid, minWidth, maxWidth));
 
-				xShift = (align == 0 ? barWid / 2 : align == _dirX ? 0 : barWid) - align * _dirX * ((align == 0 ? extraGap / 2 : 0) + (insetStroke ? strokeWidth / 2 : 0));
+				xShift = (_align == 0 ? barWid / 2 : _align == _dirX ? 0 : barWid) - _align * _dirX * ((_align == 0 ? extraGap / 2 : 0) + (insetStroke ? strokeWidth / 2 : 0));
 
 
 				const _paths = {stroke: null, fill: null, clip: null, band: null, gaps: null, flags: 0};  // disp, geom

src/paths/bars.js
@@ -55,6 +55,7 @@ export function bars(opts) {
 	return (u, seriesIdx, idx0, idx1) => {
 		return orient(u, seriesIdx, (series, dataX, dataY, scaleX, scaleY, valToPosX, valToPosY, xOff, yOff, xDim, yDim) => {
 			let pxRound = series.pxRound;
+			let _align = align;
 
 			let extraGap = _extraGap * pxRatio;
 			let maxWidth = _maxWidth * pxRatio;
@@ -118,6 +119,7 @@ export function bars(opts) {
 			let { x0, size } = disp;
 
 			if (x0 != null && size != null) {
+				_align = 1;
 				dataX = x0.values(u, seriesIdx, idx0, idx1);
 
 				if (x0.unit == 2)
@@ -161,7 +163,7 @@ export function bars(opts) {
 
 			barWid = pxRound(clamp(rawBarWid, minWidth, maxWidth));
 
-			xShift = (align == 0 ? barWid / 2 : align == _dirX ? 0 : barWid) - align * _dirX * ((align == 0 ? extraGap / 2 : 0) + (insetStroke ? strokeWidth / 2 : 0));
+			xShift = (_align == 0 ? barWid / 2 : _align == _dirX ? 0 : barWid) - _align * _dirX * ((_align == 0 ? extraGap / 2 : 0) + (insetStroke ? strokeWidth / 2 : 0));
 
 
 			const _paths = {stroke: null, fill: null, clip: null, band: null, gaps: null, flags: 0};  // disp, geom