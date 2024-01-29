leeoniya/uPlot

commit 378faf6fab9b84d86fd25a5b4425dc44d486b64d

only copy data in setData() when x scale is ordinal. close #890.

dist/uPlot.cjs.js
@@ -3642,31 +3642,36 @@ function uPlot(opts, data, then) {
 	let viaAutoScaleX = false;
 
 	function setData(_data, _resetScales) {
-		data = _data == null ? [] : copy(_data, fastIsObj);
+		data = _data == null ? [] : _data;
 
 		if (mode == 2) {
 			dataLen = 0;
 			for (let i = 1; i < series.length; i++)
 				dataLen += data[i][0].length;
-			self.data = data = _data;
+
+			self._data = data;
 		}
 		else {
-			if (data[0] == null)
-				data[0] = [];
-
-			self.data = data.slice();
+			if (data.length == 0)
+				data = [[]];
 
 			data0 = data[0];
 			dataLen = data0.length;
 
+			let scaleData = data;
+
 			if (xScaleDistr == 2) {
-				data[0] = Array(dataLen);
+				scaleData = data.slice();
+
+				let _data0 = scaleData[0] = Array(dataLen);
 				for (let i = 0; i < dataLen; i++)
-					data[0][i] = i;
+					_data0[i] = i;
 			}
+
+			self._data = data = scaleData;
 		}
 
-		self._data = data;
+		self.data = data;
 
 		resetYSeries(true);
 

dist/uPlot.esm.js
@@ -3640,31 +3640,36 @@ function uPlot(opts, data, then) {
 	let viaAutoScaleX = false;
 
 	function setData(_data, _resetScales) {
-		data = _data == null ? [] : copy(_data, fastIsObj);
+		data = _data == null ? [] : _data;
 
 		if (mode == 2) {
 			dataLen = 0;
 			for (let i = 1; i < series.length; i++)
 				dataLen += data[i][0].length;
-			self.data = data = _data;
+
+			self._data = data;
 		}
 		else {
-			if (data[0] == null)
-				data[0] = [];
-
-			self.data = data.slice();
+			if (data.length == 0)
+				data = [[]];
 
 			data0 = data[0];
 			dataLen = data0.length;
 
+			let scaleData = data;
+
 			if (xScaleDistr == 2) {
-				data[0] = Array(dataLen);
+				scaleData = data.slice();
+
+				let _data0 = scaleData[0] = Array(dataLen);
 				for (let i = 0; i < dataLen; i++)
-					data[0][i] = i;
+					_data0[i] = i;
 			}
+
+			self._data = data = scaleData;
 		}
 
-		self._data = data;
+		self.data = data;
 
 		resetYSeries(true);
 

dist/uPlot.iife.js
@@ -3643,31 +3643,36 @@ var uPlot = (function () {
 		let viaAutoScaleX = false;
 
 		function setData(_data, _resetScales) {
-			data = _data == null ? [] : copy(_data, fastIsObj);
+			data = _data == null ? [] : _data;
 
 			if (mode == 2) {
 				dataLen = 0;
 				for (let i = 1; i < series.length; i++)
 					dataLen += data[i][0].length;
-				self.data = data = _data;
+
+				self._data = data;
 			}
 			else {
-				if (data[0] == null)
-					data[0] = [];
-
-				self.data = data.slice();
+				if (data.length == 0)
+					data = [[]];
 
 				data0 = data[0];
 				dataLen = data0.length;
 
+				let scaleData = data;
+
 				if (xScaleDistr == 2) {
-					data[0] = Array(dataLen);
+					scaleData = data.slice();
+
+					let _data0 = scaleData[0] = Array(dataLen);
 					for (let i = 0; i < dataLen; i++)
-						data[0][i] = i;
+						_data0[i] = i;
 				}
+
+				self._data = data = scaleData;
 			}
 
-			self._data = data;
+			self.data = data;
 
 			resetYSeries(true);
 

src/uPlot.js
@@ -1102,31 +1102,36 @@ export default function uPlot(opts, data, then) {
 	let viaAutoScaleX = false;
 
 	function setData(_data, _resetScales) {
-		data = _data == null ? [] : copy(_data, fastIsObj);
+		data = _data == null ? [] : _data;
 
 		if (mode == 2) {
 			dataLen = 0;
 			for (let i = 1; i < series.length; i++)
 				dataLen += data[i][0].length;
-			self.data = data = _data;
+
+			self._data = data;
 		}
 		else {
-			if (data[0] == null)
-				data[0] = [];
-
-			self.data = data.slice();
+			if (data.length == 0)
+				data = [[]];
 
 			data0 = data[0];
 			dataLen = data0.length;
 
+			let scaleData = data;
+
 			if (xScaleDistr == 2) {
-				data[0] = Array(dataLen);
+				scaleData = data.slice();
+
+				let _data0 = scaleData[0] = Array(dataLen);
 				for (let i = 0; i < dataLen; i++)
-					data[0][i] = i;
+					_data0[i] = i;
 			}
+
+			self._data = data = scaleData;
 		}
 
-		self._data = data;
+		self.data = data;
 
 		resetYSeries(true);
 

commit 4cc4d150b6e0e196f58e1b5feaa5ebf113454cc9

fix distr: 2 text x labels regression from 378faf6fab9b84d86fd25a5b4425dc44d486b64d external u.data must be the original one; internal data and u._data is numeric

dist/uPlot.cjs.js
@@ -3713,16 +3713,16 @@ function uPlot(opts, data, then) {
 	function setData(_data, _resetScales) {
 		data = _data == null ? [] : _data;
 
+		self.data = self._data = data;
+
 		if (mode == 2) {
 			dataLen = 0;
 			for (let i = 1; i < series.length; i++)
 				dataLen += data[i][0].length;
-
-			self._data = data;
 		}
 		else {
 			if (data.length == 0)
-				data = [[]];
+				self.data = self._data = data = [[]];
 
 			data0 = data[0];
 			dataLen = data0.length;
@@ -3739,8 +3739,6 @@ function uPlot(opts, data, then) {
 
 			self._data = data = scaleData;
 		}
-
-		self.data = data;
 
 		resetYSeries(true);
 

dist/uPlot.esm.js
@@ -3711,16 +3711,16 @@ function uPlot(opts, data, then) {
 	function setData(_data, _resetScales) {
 		data = _data == null ? [] : _data;
 
+		self.data = self._data = data;
+
 		if (mode == 2) {
 			dataLen = 0;
 			for (let i = 1; i < series.length; i++)
 				dataLen += data[i][0].length;
-
-			self._data = data;
 		}
 		else {
 			if (data.length == 0)
-				data = [[]];
+				self.data = self._data = data = [[]];
 
 			data0 = data[0];
 			dataLen = data0.length;
@@ -3737,8 +3737,6 @@ function uPlot(opts, data, then) {
 
 			self._data = data = scaleData;
 		}
-
-		self.data = data;
 
 		resetYSeries(true);
 

dist/uPlot.iife.js
@@ -3714,16 +3714,16 @@ var uPlot = (function () {
 		function setData(_data, _resetScales) {
 			data = _data == null ? [] : _data;
 
+			self.data = self._data = data;
+
 			if (mode == 2) {
 				dataLen = 0;
 				for (let i = 1; i < series.length; i++)
 					dataLen += data[i][0].length;
-
-				self._data = data;
 			}
 			else {
 				if (data.length == 0)
-					data = [[]];
+					self.data = self._data = data = [[]];
 
 				data0 = data[0];
 				dataLen = data0.length;
@@ -3740,8 +3740,6 @@ var uPlot = (function () {
 
 				self._data = data = scaleData;
 			}
-
-			self.data = data;
 
 			resetYSeries(true);
 

src/uPlot.js
@@ -1194,16 +1194,16 @@ export default function uPlot(opts, data, then) {
 	function setData(_data, _resetScales) {
 		data = _data == null ? [] : _data;
 
+		self.data = self._data = data;
+
 		if (mode == 2) {
 			dataLen = 0;
 			for (let i = 1; i < series.length; i++)
 				dataLen += data[i][0].length;
-
-			self._data = data;
 		}
 		else {
 			if (data.length == 0)
-				data = [[]];
+				self.data = self._data = data = [[]];
 
 			data0 = data[0];
 			dataLen = data0.length;
@@ -1220,8 +1220,6 @@ export default function uPlot(opts, data, then) {
 
 			self._data = data = scaleData;
 		}
-
-		self.data = data;
 
 		resetYSeries(true);
 