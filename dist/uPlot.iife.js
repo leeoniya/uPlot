f98d1d34d92a128f18fbaac9293910eed85ea19c
Leon Sorokin <leeoniya@gmail.com>
2024-09-27 21:38:11 -0500

    1.6.31

--- dist/uPlot.iife.js
+++ dist/uPlot.iife.js
@@ -4,7 +4,7 @@
 *
 * uPlot.js (μPlot)
 * A small, fast chart for time series, lines, areas, ohlc & bars
-* https://github.com/leeoniya/uPlot (v1.6.30)
+* https://github.com/leeoniya/uPlot (v1.6.31)
 */
 
 var uPlot = (function () {
