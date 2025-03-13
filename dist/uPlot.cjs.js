leeoniya/uPlot

commit b24436df67cfa393cc7e51a5b328288dec6cfe49

accept HTMLElement for series.label

dist/uPlot.cjs.js
@@ -3238,7 +3238,11 @@ function uPlot(opts, data, then) {
 		}
 
 		let text = placeDiv(LEGEND_LABEL, label);
-		text.textContent = s.label;
+
+		if (isStr(s.label))
+			text.textContent = s.label;
+		else
+			text.appendChild(s.label);
 
 		if (i > 0) {
 			if (!markers.show)

dist/uPlot.esm.js
@@ -3236,7 +3236,11 @@ function uPlot(opts, data, then) {
 		}
 
 		let text = placeDiv(LEGEND_LABEL, label);
-		text.textContent = s.label;
+
+		if (isStr(s.label))
+			text.textContent = s.label;
+		else
+			text.appendChild(s.label);
 
 		if (i > 0) {
 			if (!markers.show)

dist/uPlot.iife.js
@@ -3239,7 +3239,11 @@ var uPlot = (function () {
 			}
 
 			let text = placeDiv(LEGEND_LABEL, label);
-			text.textContent = s.label;
+
+			if (isStr(s.label))
+				text.textContent = s.label;
+			else
+				text.appendChild(s.label);
 
 			if (i > 0) {
 				if (!markers.show)

src/uPlot.js
@@ -627,7 +627,11 @@ export default function uPlot(opts, data, then) {
 		}
 
 		let text = placeDiv(LEGEND_LABEL, label);
-		text.textContent = s.label;
+
+		if (isStr(s.label))
+			text.textContent = s.label;
+		else
+			text.appendChild(s.label);
 
 		if (i > 0) {
 			if (!markers.show)

dist/uPlot.d.ts
@@ -909,7 +909,7 @@ declare namespace uPlot {
 		pxAlign?: number | boolean; // 1
 
 		/** legend label */
-		label?: string;
+		label?: string | HTMLElement;
 
 		/** inline-legend value formatter. can be an fmtDate formatting string when scale.time: true */
 		value?: Series.Value;

commit 39e77ec73398ce483ba0237f6d28d97cb79e240a

fix numeric legend labels. (regressed in b24436d)

dist/uPlot.cjs.js
@@ -3249,10 +3249,10 @@ function uPlot(opts, data, then) {
 
 		let text = placeDiv(LEGEND_LABEL, label);
 
-		if (isStr(s.label))
-			text.textContent = s.label;
-		else
+		if (s.label instanceof HTMLElement)
 			text.appendChild(s.label);
+		else
+			text.textContent = s.label;
 
 		if (i > 0) {
 			if (!markers.show)

dist/uPlot.esm.js
@@ -3247,10 +3247,10 @@ function uPlot(opts, data, then) {
 
 		let text = placeDiv(LEGEND_LABEL, label);
 
-		if (isStr(s.label))
-			text.textContent = s.label;
-		else
+		if (s.label instanceof HTMLElement)
 			text.appendChild(s.label);
+		else
+			text.textContent = s.label;
 
 		if (i > 0) {
 			if (!markers.show)

dist/uPlot.iife.js
@@ -3250,10 +3250,10 @@ var uPlot = (function () {
 
 			let text = placeDiv(LEGEND_LABEL, label);
 
-			if (isStr(s.label))
-				text.textContent = s.label;
-			else
+			if (s.label instanceof HTMLElement)
 				text.appendChild(s.label);
+			else
+				text.textContent = s.label;
 
 			if (i > 0) {
 				if (!markers.show)

src/uPlot.js
@@ -634,10 +634,10 @@ export default function uPlot(opts, data, then) {
 
 		let text = placeDiv(LEGEND_LABEL, label);
 
-		if (isStr(s.label))
-			text.textContent = s.label;
-		else
+		if (s.label instanceof HTMLElement)
 			text.appendChild(s.label);
+		else
+			text.textContent = s.label;
 
 		if (i > 0) {
 			if (!markers.show)