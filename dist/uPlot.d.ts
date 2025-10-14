a9db2f8b4836187ec8a42835ececd12d9b75eb4b
Marc Gállego <mga2525@gmail.com>
2025-10-14 13:39:19 +0200

    Safer approach to setScale range

--- dist/uPlot.d.ts
+++ dist/uPlot.d.ts
@@ -77,7 +77,7 @@ declare class uPlot {
 	setData(data: uPlot.AlignedData, resetScales?: boolean): void;
 
 	/** sets the limits of a scale & redraws (used for zooming) */
-	setScale(scaleKey: string, limits: { min: number | null; max: number | null }): void;
+	setScale(scaleKey: string, limits: ({ min: number; max: number } | { min: null; max: null })): void;
 
 	/** sets the cursor position (relative to plotting area) */
 	setCursor(opts: {left: number, top: number}, fireHook?: boolean): void;
